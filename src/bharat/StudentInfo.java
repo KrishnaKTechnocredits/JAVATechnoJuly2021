076700910555f453fe7b46124b1b6c52027ba778
Your Namebharat git config --global user.name Your <bharat.tanpure777@gmail.com>
2021-08-07 23:07:26 +0530

    BharatTanpureAssignment

---
+++ src/bharat/StudentInfo.java
@@ -0,0 +1,20 @@
+package bharat;
+class StudentInfo{
+	
+	
+	void studentName(String studentname, String midldeName, String surName){
+		System.out.println(studentname);
+		System.out.println(midldeName);
+		System.out.println(surName);
+	}
+	void studentOtherDetails(String birthDate, String address, int rollNumber){
+		System.out.println(birthDate);
+		System.out.println(address);
+		System.out.println(rollNumber);
+	}
+	public static void main(String args[]){
+		StudentInfo studentinfo= new StudentInfo();
+		studentinfo.studentName("Bharat","Ramrao","Tanpure");
+		studentinfo.studentOtherDetails("7 November 1995", "FT 505 Pune Ngar hyway Pune", 20);
+	}
+}
