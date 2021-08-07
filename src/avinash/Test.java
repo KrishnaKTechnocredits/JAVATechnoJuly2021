cf0aa951aa00c44609fdb3a992bc56eb5609108a
Avinash Patil <avinashspatil12@gmail.com>
2021-08-07 11:08:39 +0530

    Avinash_Assignment1_1

---
+++ src/avinash/Test.java
@@ -0,0 +1,13 @@
+package avinash;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name = "Avinash";
+		System.out.println(name);
+
+	}
+
+}
