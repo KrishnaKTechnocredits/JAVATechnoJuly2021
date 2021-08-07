a8c08e2c5e0679c6125ae1c91eb289f60315fd83
Chandni Fatehchandani <chandnif6@gmail.com>
2021-08-07 11:09:27 +0530

    Chandani_Assignment_1

---
+++ src/chandani/FirstProgram.java
@@ -0,0 +1,11 @@
+package chandani;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Chandani";
+		System.out.println("My Name is "+ name);
+	}
+
+}
