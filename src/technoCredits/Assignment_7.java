f60d51f1b31adf7603ad1ff929dc6cb66c30653f
Krishna Kanani <kananikrishna427@gmail.com>
2021-08-09 07:54:51 +0530

    Krishna_Assignment_7_correction

--- src/technoCredits/Assignment_7.java
+++ src/technoCredits/Assignment_7.java
@@ -3,6 +3,8 @@ package technoCredits;
 public class Assignment_7 {
 
 	public static void main(String[] args) {
-		System.out.println("Assignment7   ");
+		System.out.println("Assignment7");
+		System.out.println("Assignment");
+		System.out.println("Assignment");
 	}
 }
