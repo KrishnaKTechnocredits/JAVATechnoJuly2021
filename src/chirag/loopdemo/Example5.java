b55cc6afaafd9208176b030236918cf671f46eb3
Chirag Prajapati <chirag.68.cp@gmail.com>
2021-08-09 15:56:03 +0530

    Chirag_Assignment_7

---
+++ src/chirag/loopdemo/Example5.java
@@ -0,0 +1,14 @@
+package chirag.loopdemo;
+
+public class Example5 {
+	void display() {
+		for(int index=1;index<=5;index++) {
+			System.out.println("Chirag");
+		}
+	}
+	public static void main(String[] args) {
+		Example5 example5=new Example5();
+		example5.display();
+	}
+
+}
