dddce95a92f429c607e67eab8993929c6728a424
Ramkrishna <ramu.nikam@gamil.com>
2021-08-09 08:22:29 +0530

    Ramkrishna_Assignement_6

---
+++ src/ramkrishna/SimpleInterest.java
@@ -0,0 +1,39 @@
+
+package ramkrishna;
+
+	public class SimpleInterest {
+
+		int s1;
+	    int d;
+		void simpleInterset(int P, int T, int R) {
+			
+			int s2 = (P*T*R)/100;
+			s1=s2; 
+			System.out.println("Simple Interset is : " +s2);
+		}
+
+		void convertTemp(int f) {
+			int c = (f - 32) * 5 / 9;
+			int d= c;
+			System.out.println("Convernt Temprature is: "+c);
+
+		}
+
+		void displayInfo() {
+			System.out.println("Simple Interset is : " + s1);
+			System.out.println("Convert Tempreature is "+ d);
+
+		}
+
+		public static void main(String[] args) {
+
+			SimpleInterest simpleinterest = new SimpleInterest();
+			simpleinterest.simpleInterset(2000, 3, 7);
+			simpleinterest.convertTemp(56);
+			simpleinterest.displayInfo();
+
+		}
+
+	}
+
+
