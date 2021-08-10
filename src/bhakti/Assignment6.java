743c0f11f1348c3885d90e5fdb3dc479c42d10a2
Bhakti Kakade <bskakade@gmail.com>
2021-08-10 16:30:49 +0530

    BhaktK_Assignment 6new

--- src/bhakti/Assignment6.java
+++ src/bhakti/Assignment6.java
@@ -1,6 +1,6 @@
 package bhakti;
 
-public class Assignment7 {
+public class Assignment6 {
 	
 	void simpleInterest(float p, int t, float r) {
 		float smpInt= (p*t*r)/100;
@@ -16,10 +16,9 @@ public class Assignment7 {
 	}
 	 
 	public static void main(String[] df) {
-		Assignment7 assignment7= new Assignment7();
-		assignment7.simpleInterest(3444.78f, 2016, 23.45f);
-		assignment7.temToFah(34.56f);
-		
+		Assignment6 assignment6= new Assignment6();
+		assignment6.simpleInterest(3444.78f, 2016, 23.45f);
+		assignment6.temToFah(34.56f);
 	}
 
 }
