0896d2d80faadbb33514591de68a3298c27fc334
Elizabeth Mary <elizabethjems@gmail.com>
2021-08-11 18:09:20 +0530

    Elizabeth_Assignment_9

--- src/elizabeth/Assignment9.java
+++ src/elizabeth/Assignment9.java
@@ -12,6 +12,7 @@ Expectation:
 3) Create a method which shows only those item which is "Available in stock" & respective quantities.*/
 
 package elizabeth;
+
 class Assignment9{
 	
 	int maggieCount;
@@ -19,6 +20,13 @@ class Assignment9{
 	int oilPouchesCount;
 	int panipuriCount;
 	int masalaCount;
+	
+	int requestedMaggie;
+	int requestedDosa;
+	int requestedOilPouches;
+	int requestedPanipuri;
+	int requestedMasala;
+	
 	
 	void inStock(int maggie,int dosa,int oilPouches,int panipuri,int masala){
 		
@@ -30,54 +38,85 @@ class Assignment9{
 	}
 
 	void itemQuantity(int orderMaggie,int orderDosa,int orderOilPouches,int orderPanipuri,int orderMasala){
+		
+		requestedMaggie=orderMaggie;
+		requestedDosa=orderDosa;
+		requestedOilPouches=orderOilPouches;
+		requestedPanipuri=orderPanipuri;
+		requestedMasala=orderMasala;
+		
 		System.out.println("MAGGIE:");
 		if(orderMaggie<=maggieCount){
-			int remainingQuantity=maggieCount-orderMaggie;
 			System.out.println("Maggies are available in stock");
-			System.out.println("After selling remaining stock is " +remainingQuantity);
-			
 		}else{
 			System.out.println("running out of stock");
 		}
 		System.out.println("DOSA:");
 		if(orderDosa<=dosaCount){
-			int remainingQuantity=dosaCount-orderDosa;
 			System.out.println("Dosas are available in stock");
-			System.out.println("After selling remaining stock is " +remainingQuantity);
 		}else{
 			System.out.println("running out of stock");
 		}
 		System.out.println("OIL POUCHES:");
 		if(orderOilPouches<=oilPouchesCount){
-			int remainingQuantity=oilPouchesCount-orderOilPouches;
 			System.out.println("Oil Pouches are available in stock");
-			System.out.println("After selling remaining stock is " +remainingQuantity);
 		}else{
 			System.out.println("running out of stock");
 		}
 		System.out.println("PANIPURI:");
 		if(orderPanipuri<=panipuriCount){
-			int remainingQuantity=panipuriCount-orderPanipuri;
 			System.out.println("Panipuries are available in stock");
-			System.out.println("After selling remaining stock is " +remainingQuantity);
 		}else{
 			System.out.println("running out of stock");
 		}
 		System.out.println("MASALA:");
 		if(orderMasala<=masalaCount){
-			int remainingQuantity=masalaCount-orderMasala;
 			System.out.println("Masalas are available in stock");
-			System.out.println("After selling remaining stock is " +remainingQuantity);
 		}else{
 			System.out.println("running out of stock");
+		}
+	}
+	
+	void remainingQuantity(){
+		
+		System.out.println("*********Remaining Stock**********");
+		if(requestedMaggie<=maggieCount){
+		int remainingMaggie=maggieCount-requestedMaggie;
+		System.out.println("After selling remaining maggie is " +remainingMaggie);
+		}else{
+			System.out.println("Item sold out");
+		}
+		if(requestedDosa<=dosaCount){
+		int remainingDosa=dosaCount-requestedDosa;
+		System.out.println("After selling remaining dosa is " +remainingDosa);
+		}else{
+			System.out.println("Item sold out");
+		}
+		if(requestedOilPouches<=oilPouchesCount){
+		int remainingOilPouches=oilPouchesCount-requestedOilPouches;
+		System.out.println("After selling remaining oilPouches is " +remainingOilPouches);
+		}else{
+			System.out.println("Item sold out");
+		}
+		if(requestedPanipuri<=panipuriCount){
+		int remainingPanipuri=panipuriCount-requestedPanipuri;
+		System.out.println("After selling remaining panipuri is " +remainingPanipuri);
+		}else{
+			System.out.println("Item sold out");
+		}
+		if(requestedMasala<=masalaCount){
+		int remainingMasala=masalaCount-requestedMasala;
+		System.out.println("After selling remaining masala is " +remainingMasala);	
+		}else{
+			System.out.println("Item sold out");
 		}
 	}
 	
 	public static void main(String[] args){
 		Assignment9 assignment9=new Assignment9();
 		assignment9.inStock(50,43,39,43,73);
-		assignment9.itemQuantity(20,30,40,30,50);
+		assignment9.itemQuantity(45,30,30,30,75);
+		assignment9.remainingQuantity();
 	}
-	
 	
 }
