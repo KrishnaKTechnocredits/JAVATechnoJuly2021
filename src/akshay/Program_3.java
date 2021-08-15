ccbf07c0ee77058cdf2f6adcc8af98ab7c98322b
Akshay Malshetwar <akshaymalshetwar@gmail.com>
2021-08-15 23:56:36 +0530

    Akshay_Assignment_13

---
+++ src/akshay/Program_3.java
@@ -0,0 +1,25 @@
+package akshay;
+
+public class Program_3 {
+	
+	int[] getArrayofFiveNumbers(int num) {
+		
+		int count = 0;
+		int[] input = new int[5];
+		for(int index = num; index > num- 5; index--) {
+			if(count != 5) {
+				input[count] = index - 1;
+				System.out.println(input[count]);
+				count++;
+			}
+		}
+		return input;
+	}
+	
+	public static void main(String[] args) {
+		Program_3 program3 = new Program_3();
+		program3.getArrayofFiveNumbers(50);
+		
+	}
+	
+}
