3e5716288be9ca5280187a9d18286de06eb1495a
Chandni Fatehchandani <chandnif6@gmail.com>
2021-08-16 10:18:26 +0530

    Chandani_Assignment_13

--- src/chandani/Assignment13a.java
+++ src/chandani/Assignment13a.java
@@ -2,19 +2,20 @@ package chandani;
 
 class Assignment13a{
 	
-	void getPositiveNumber(int[] arr){
+	int getPositiveNumber(int[] arr){
 		int count = 0;
 		for(int index = 0; index < arr.length; index++){
 			if(arr[index]>0){
 				count++;
 			}
 		}
-		System.out.println("Positive numbers in given array are " + count);
+		return count;
 	}
 	
 	public static void main(String[] args){
 		Assignment13a assignment13a = new Assignment13a();
 		int[] arr = new int[]{1,-11,-44,23,55,-99,-23,-22};
-		assignment13a.getPositiveNumber(arr);
+		System.out.println("Positive numbers in given array are " + assignment13a.getPositiveNumber(arr));
+
 	}
 }
