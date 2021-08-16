e46dac391b3c685dea7eb3289e17e2dcafaa134f
Chandni Fatehchandani <chandnif6@gmail.com>
2021-08-16 10:21:19 +0530

    Chandani_Assignment_13

--- src/chandani/Assignment13b.java
+++ src/chandani/Assignment13b.java
@@ -2,19 +2,19 @@ package chandani;
 
 class Assignment13b{
 	
-	void getCountNumber0(int[] arr){
+	int getCountNumber0(int[] arr){
 		int count = 0;
 		for(int index = 0; index < arr.length; index++){
 			if(arr[index]==0){
 				count++;
 			}
 		}
-		System.out.println("Number of 0's in a given array are " + count);
+		return count;
 	}
 	
 	public static void main(String[] args){
 		Assignment13b assignment13b = new Assignment13b();
 		int[] arr = new int[]{1,-11,0,0,55,0,-23,0};
-		assignment13b.getCountNumber0(arr);
+		System.out.println("Number of 0's in a given array are " + assignment13b.getCountNumber0(arr));
 	}
 }
