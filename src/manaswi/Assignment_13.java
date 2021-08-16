b40a2589caec12a9e2b0693c9f4e66d5795d1661
Manaswi Humbe <manaswi1010@gmail.com>
2021-08-16 21:35:35 +0530

    Manaswi_Assignment_13

--- src/manaswi/Assignment_13.java
+++ src/manaswi/Assignment_13.java
@@ -1,28 +1,36 @@
 package manaswi;
 
-class Assignment_13 {
+class Assignment_13 
+{
 
-	int countPositive(int[] num){
+	int countPositive(int[] num)
+	{
 		int count=0;
-		for(int index =0; index < num.length; index++) {
-			if(num[index] > 0) {
+		for(int index =0; index < num.length; index++)
+		{
+			if(num[index] > 0)
+			{
 				count++;
 			}
 		}
 		return count;
 	}
-	int countZeroNumber(int[] num) {
+	int countZeroNumber(int[] num)
+	{
 		int count=0;
-		for(int index=0;index<num.length;index++) {
+		for(int index=0;index<num.length;index++)
+		{
 			if(num[index]==0)
 				count++;
 		}
 		return count;
 	}
-	int[] fiveSmallerNumbers(int num) {
+	int[] fiveSmallerNumbers(int num) 
+	{
 		int[] input = new int[5];
 		int count = 0;
-		for (int index = num; index > num - 5; num--) {
+		for (int index = num; index > num - 5; num--) 
+		{
 			if (count != 5) {
 				input[count] = num - 1;
 				System.out.println(input[count]);
@@ -34,7 +42,8 @@ class Assignment_13 {
 
 
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		int[] input = {1,-11,0,0,55,0,-23,0};
 		int[] positive = {1,-11,-44,23,55,-99,-23,-22};
 		Assignment_13 assignment_13 = new Assignment_13();
