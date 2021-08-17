85ea8dac2f7a40aa70971953ffaf09373c44e8c4
Varun Joshi <varunjoshi8822@gmail.com>
2021-08-17 18:42:56 +0530

    Varun_Assignment_14

---
+++ src/varun_array_assignment/Maximum_Number_From_Array_Assignment_14.java
@@ -0,0 +1,24 @@
+/*program 1 : find maximum number from given array.
+
+int[] input = {1,11,44,23,55,99,23};
+output : 99*/
+
+package varun_array_assignment;
+
+public class Maximum_Number_From_Array_Assignment_14 {
+	int max = 0;
+	void maxNumber(int []input) {
+		max = input[0];
+		for(int index = 0;index<input.length;index++) {
+			if(max<input[index])
+				max = input[index];
+		}
+		System.out.println("Maximum number from the given array is " +max);
+	}
+	
+	public static void main(String[] args) {
+		Maximum_Number_From_Array_Assignment_14 assignment_14 = new Maximum_Number_From_Array_Assignment_14();
+		int[] input = {1,11,44,23,55,99,23};
+		assignment_14.maxNumber(input);
+	}
+}
