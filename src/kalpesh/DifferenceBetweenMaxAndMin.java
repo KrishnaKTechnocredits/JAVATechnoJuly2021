4cde19b6026baa7cbcccc7c809463a88db61abcc
Kalpesh Chaudhari <getkalpeshchaudhari10@gmail.com>
2021-08-17 23:25:37 +0530

    Kalpesh_Assignment_14

---
+++ src/kalpesh/DifferenceBetweenMaxAndMin.java
@@ -0,0 +1,67 @@
+package kalpesh;
+
+public class DifferenceBetweenMaxAndMin {
+
+	static int max = 0, min = 100;
+
+	int findMaxNumber(int[] input) {
+		for (int index = 0; index < input.length; index++) {
+			if (input[index] > max)
+				max = input[index];
+		}
+		return max;
+	}
+
+	int findMinNumber(int[] input) {
+		for (int index = 0; index < input.length; index++) {
+			if (input[index] < min)
+				min = input[index];
+		}
+		return min;
+	}
+
+	public static void main(String[] args) {
+		DifferenceBetweenMaxAndMin difference = new DifferenceBetweenMaxAndMin();
+		int[] input = { 1, 11, 44, 23, 55, 99, 23, };
+		difference.findMaxNumber(input);
+		difference.findMinNumber(input);
+		int diff;
+		diff = max - min;
+		System.out.println("The difference between max & min numbers is " + diff);
+	}
+
+}
+
+
+/* public class Assignment_14_Program_4 {
+	static int maxEven=0, maxOdd=0, diff;
+
+	void findMaxEvenOddNumber(int[] input) {
+		for(int index = 0;index<input.length;index++){
+			if(input[index]%2==0)
+				if (input[index]>maxEven)
+						maxEven = input[index];
+			if(input[index]%2!=0)
+				if (input[index]>maxOdd) 
+					maxOdd = input[index];
+		}
+		System.out.println("The maximum odd number in array is: " + maxOdd);
+		System.out.println("The maximum even number in array is: " + maxEven);
+	}
+
+	static int diff(int maxEven, int maxOdd) {
+		if (maxOdd>maxEven)
+			diff= maxOdd - maxEven;
+		else
+			diff = maxEven - maxOdd;
+		return diff;
+	}
+
+	public static void main(String[] args) {
+		int[] input = {1,11,44,23,55,99,23};
+		Assignment_14_Program_4 program_4 = new Assignment_14_Program_4();
+		program_4.findMaxEvenOddNumber(input);
+		Assignment_14_Program_4.diff(maxEven, maxOdd);
+		System.out.println("The difference between maximum odd number and maximun even number from given array is " + diff);
+	}
+}*/
