7cdb3b89133a16d1b48cd4b50990912d4bb35555
Aniket Balsaraf <aniketbalsaraf08@gmail.com>
2021-08-18 09:44:53 +0530

    Aniket_Assignment_14

---
+++ src/aniket/MaxNumberArray.java
@@ -0,0 +1,20 @@
+package aniket;
+
+public class MaxNumberArray {
+
+	int maxNumberArray(int[] num) {
+		int maxNumber = num[0];
+		for(int index = 0; index < num.length; index++) {
+			if(num[index] > maxNumber)
+				maxNumber = num[index];
+		}	
+		return maxNumber;
+	}
+
+	public static void main(String[] args) {
+		MaxNumberArray maxnumber = new MaxNumberArray();
+		int[] input = {1,11,44,23,55,99,23};
+
+		System.out.println("Biggest Number from Array: " +maxnumber.maxNumberArray(input));
+	}
+}
