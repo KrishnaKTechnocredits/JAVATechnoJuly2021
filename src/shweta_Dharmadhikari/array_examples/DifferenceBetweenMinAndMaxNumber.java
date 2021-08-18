085cb483ac7172fc95a7bbdc149382669ba8d2f8
Shweta Dharmadhikari <shweta.dharmadhikari7@gmail.com>
2021-08-18 18:41:25 +0530

    ShwetaD_Assignment_13

---
+++ src/shweta_Dharmadhikari/array_examples/DifferenceBetweenMinAndMaxNumber.java
@@ -0,0 +1,27 @@
+package shweta_Dharmadhikari.array_examples;
+
+public class DifferenceBetweenMinAndMaxNumber {
+	int[] input = { 1, 11, 44, 23, 55, 99, 23, 22 };
+	int index = 0;
+	int max = input[index];
+	int min = input[index];
+
+	int diffBetweenMaxOddAndMaxEvenNumber() {
+		for (; index < input.length; index++) {
+			if (input[index] > max && input[index] % 2 == 0)
+				max = input[index];
+			else if (input[index] > min && input[index] % 2 != 0)
+				min = input[index];
+		}
+		if (max > min)
+			return max - min;
+		else
+			return min - max;
+	}
+
+	public static void main(String[] args) {
+		DifferenceBetweenMinAndMaxNumber differenceBetweenMinAndMaxNumber = new DifferenceBetweenMinAndMaxNumber();
+		System.out.println("Difference between max odd number and max even number is :" + differenceBetweenMinAndMaxNumber.diffBetweenMaxOddAndMaxEvenNumber());
+	}
+
+}
