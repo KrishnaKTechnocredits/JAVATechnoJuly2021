1cccb7d0b2f36d1511feb89db9e14fd8644cb9d4
Precilla Aranha <precillaaranha16@gmail.com>
2021-08-19 14:40:39 +0530

    Precilla_Assignment18

---
+++ src/precilla/ReverseStringPrograms.java
@@ -0,0 +1,40 @@
+package precilla;
+
+import java.util.Arrays;
+
+public class ReverseStringPrograms {
+	String reverseStringArray(String[] array) {
+		String[] reverse=new String[array.length];
+		for(int index=0;index<array.length;index++) {
+			String reverse1="";
+			for(int index1=array[index].length()-1;index1>=0;index1--) {
+				reverse1=reverse1+array[index].charAt(index1);
+				reverse[index]=reverse1;
+				}
+			}
+		return Arrays.toString(reverse);
+	}
+	String reverseStringArray1(String[] array) {	
+		String[] reverse=new String[array.length];
+		int indexReverse=0;
+		for(int index=0;index<array.length;index++)  {
+			String reverse1="";
+			for(int index1=array[index].length()-1;index1>=0;index1--) {
+				reverse1=reverse1+array[index].charAt(index1);
+				reverse[indexReverse]=reverse1;
+				}
+			indexReverse++;
+			}
+		return Arrays.toString(reverse);
+	}
+	public static void main(String[] args) {
+		ReverseStringPrograms reverseArray=new ReverseStringPrograms();
+		String array[]= {"madhvi", "surbhi", "neha", "sandesh", "ankit", "vaibhav"};
+		String array1[]= {"Techno","credits","Precilla","Aranha"};
+		System.out.println("Program 1 :\nGiven array "+ Arrays.toString(array));
+		System.out.println("Reverse elements at same position : "+ reverseArray.reverseStringArray(array));
+		System.out.println("\nProgram 2 :\nGiven array "+ Arrays.toString(array1));
+		System.out.println("Reverse elements in a reverse order : "+ reverseArray.reverseStringArray1(array1));
+	}
+
+}
