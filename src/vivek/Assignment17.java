ddccb0d90ccbb91a5352c584f835863bc4199e88
Vivek Patil <vivekvpatil24@gmail.com>
2021-08-19 10:11:10 +0530

    Vivek_Assignment_17

---
+++ src/vivek/Assignment17.java
@@ -0,0 +1,21 @@
+package vivek;
+
+public class Assignment17 {
+
+	String getReverseString(String input) {
+
+		String output = "";
+		for (int index = input.length() - 1; index >= 0; index--) {
+			output = output + input.charAt(index);
+		}
+		return output;
+	}
+
+	public static void main(String[] args) {
+
+		Assignment17 assignment17 = new Assignment17();
+		String name = "technocredits";
+
+		System.out.println(assignment17.getReverseString(name));
+	}
+}
