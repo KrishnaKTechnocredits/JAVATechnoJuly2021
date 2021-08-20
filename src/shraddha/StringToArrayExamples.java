1ab569f57b6d7d836948d6a6a95a462284806e95
Shraddha Rathi <shraddhaatal@gmail.com>
2021-08-20 18:31:19 +0400

    Shraddha_Assignment19

--- src/shraddha/StringToArrayExamples.java
+++ src/shraddha/StringToArrayExamples.java
@@ -1,3 +1,22 @@
+/*
+ * Assignment - 19 : 20th Aug'2021
+
+program 1: Find how many words in a given statement.
+input = "Hi Techno Credits Hello"
+output : 4
+
+program 2: Find maximum word length from given statement.
+input = "good morning technocredits hi hello";
+output : technocredits
+
+program 3 : Find minimum word length from given statement.
+input = "good morning technocredits hi hello";
+output : hi
+
+program 4: sum of all numbers in given statement.
+input = "10 10 20 30";
+output : 70
+ * */
 package shraddha;
 
 public class StringToArrayExamples {
