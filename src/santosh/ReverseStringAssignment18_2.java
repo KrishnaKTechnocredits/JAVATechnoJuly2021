9e6f70d69a0759975b6e12c06365d8d6c936f178
Santosh Ghige <santoshghige1993@gmail.com>
2021-08-20 19:17:29 +0530

    Santosh_Assignment18

---
+++ src/santosh/ReverseStringAssignment18_2.java
@@ -0,0 +1,36 @@
+/* Assignment_18 :: array.Program 2 : Reverse all elements and place it in a reverse order
+String[] input = {"techno", "credits", "krishna","harsh"}
+output : [hsrah, anhsirk, stiderc, onhcet] */
+package santosh;
+import java.util.Arrays;
+public class ReverseStringAssignment18_2 {
+
+	String getReverseString(String input){
+		String output="";
+		for(int index=input.length()-1;index>=0;index--){
+			output=output+input.charAt(index);
+		}
+		return output;
+	}
+	String[] getReverseStringReverseArray(String[] input){
+		String[] output= new String[input.length];
+		int count=0;
+		for(int index=input.length-1;index>=0;index--){
+			output[count]=getReverseString(input[index]);
+			count++;
+		}
+		return output;
+	}
+	public static void main(String[] args)	{
+		String[] input={"techno","credits","krishna","harsh"};
+		ReverseStringAssignment18_2 reverseAssignment2= new ReverseStringAssignment18_2();
+		String[] output = reverseAssignment2.getReverseStringReverseArray(input);
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("\n Reverse all elements of an Array"+Arrays.toString(input)+" place it in a reverse order");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("\n Reverse Array Elements: "+Arrays.toString(output));
+	    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+	
+	}
+
+}
