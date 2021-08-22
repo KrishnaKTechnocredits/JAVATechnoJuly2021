9a7a21fb136690d0e99c34155912f2a055570e0e
Shyam Anasane <shyamanasane@gmail.com>
2021-08-22 21:03:42 +0530

    Shyam_Assignment_19

---
+++ src/shyam/StringToArrayExamples.java
@@ -0,0 +1,60 @@
+package shyam;
+
+public class StringToArrayExamples {
+	
+	public static void main (String[] args){
+		String inputString = "Hello Customer Can You Find Words In Given String";
+		StringToArrayExamples stringToArrayExamples = new StringToArrayExamples();
+		stringToArrayExamples.countOfWordsInGivenString(inputString);
+		stringToArrayExamples.biggestWordInGivenString(inputString);
+		stringToArrayExamples.smallestWordInGivenString(inputString);
+		String numberString ="10 10 20 30 40 50";
+		stringToArrayExamples.sumOfAllNumbersInString(numberString);
+	}
+	
+	void countOfWordsInGivenString(String inputString) {
+		
+		System.out.println("Input String is->" + inputString);
+		String stringArray [] = inputString.split(" ");
+		System.out.println(" Number of words in the given String is :- " + stringArray.length);
+	}
+	
+	void biggestWordInGivenString(String inputString) {
+		
+		String stringArray [] = inputString.split(" ");
+		String biggestWord = "";
+		for (int index=0; index<stringArray.length; index++) {
+			if (stringArray[index].length() > biggestWord.length())
+				biggestWord= stringArray[index];
+		}
+		System.out.println("BiggestWord in given string is->"+ biggestWord);
+		
+	}
+	
+	void smallestWordInGivenString(String inputString) {
+		
+		String[] stringArray = inputString.split(" ");
+		String smallestWord = stringArray[0];
+		for(int index = 0; index < stringArray.length; index ++) {
+			if(stringArray[index].length() < smallestWord.length())
+				smallestWord = stringArray[index];
+		}
+		System.out.println(" The smallest word in the given String is :- " + smallestWord);
+		
+	}
+	
+	void sumOfAllNumbersInString(String inputString) {
+		
+		System.out.println("Input String of nubers is :- "+ inputString);
+		String[] stringArray = inputString.split(" ");
+		int sum = 0;
+		for(int index = 0; index < stringArray.length; index ++) {
+			int number = Integer.parseInt(stringArray[index]);
+			sum += number;
+		}
+		
+		System.out.println(" The sum of all Numbers in the given String is :- " + sum);
+		
+	}
+
+}
