f265a1b50cc3c7e8f73917d6c03b9fdb16644b4d
purshottam94 <71837062+purshottam94@users.noreply.github.com>
2021-08-22 17:56:55 +0530

    Update Assignment21.java

--- src/purshottamJoshi/Assignment21.java
+++ src/purshottamJoshi/Assignment21.java
@@ -5,20 +5,18 @@ class Assignment21{
 	int ageDifference() {
 		
 		int maxAge=0;
-		int minAge=0;
 		int[] age = {10,34,38,68,72,95,6};
+		int minAge = age[0];
 		
 		for(int index=0; index<age.length; index++) {
-			if(age[index] > maxAge){
+			if(age[index] > maxAge) {
 				maxAge=age[index];
 			}
-			for(int j=0; j <age.length; j++){
-				if(age[j] < age[index]){
-					minAge = age[j];
-				}
+			if(age[index] < minAge]) {
+				minAge = age[index];
 			}
-			
 		}
+			
 		int ageDifference = maxAge - minAge;
 		return ageDifference;
 	}
