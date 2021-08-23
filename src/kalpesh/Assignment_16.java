78666d6d98fa54841fab9c9059247cac31af615b
Kalpesh Chaudhari <getkalpeshchaudhari10@gmail.com>
2021-08-23 22:34:31 +0530

    Kalpesh_Assignment_16

---
+++ src/kalpesh/Assignment_16.java
@@ -0,0 +1,29 @@
+package kalpesh;
+
+public class Assignment_16 {
+	static int count, max = 0;
+
+	int getVisibleTreesCount(int[] treeHeights) {
+		for (int index = 0; index < treeHeights.length; index++) {
+			if (treeHeights[index] > max) {
+				max = treeHeights[index];
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+		int[] treeHeights = { 3, 5, 5, 11, 9, 10, 15, 14 };
+		Assignment_16 assignment_16 = new Assignment_16();
+		assignment_16.getVisibleTreesCount(treeHeights);
+		System.out.println("The count of visible trees are " + count);
+	}
+
+}
+
+/*
+ * program 1 : From given array find out count of visible trees. [if question is
+ * not clear, please watch recording.] int[] treeHeights =
+ * {3,5,5,11,9,10,15,14}; output : 4
+ */
