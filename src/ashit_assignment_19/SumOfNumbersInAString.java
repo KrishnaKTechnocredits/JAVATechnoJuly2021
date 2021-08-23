4657448caab121037d303895b19578c5a8c526bc
Ashit46 <asamdur@gmail.com>
2021-08-23 11:55:42 +0530

    Ashit_Assignment_19

---
+++ src/ashit_assignment_19/SumOfNumbersInAString.java
@@ -0,0 +1,14 @@
+package ashit_assignment_19;
+
+public class SumOfNumbersInAString {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		String input = "10 20 30 40";
+		String [] output = input.split(" ");
+		for(int index = 0;index<output.length;index++) {
+			sum= sum + Integer.parseInt(output[index]);
+		}
+		System.out.println("Sum of numbers in a String "+sum);
+	}
+}
