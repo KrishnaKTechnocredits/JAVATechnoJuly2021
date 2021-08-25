2075652613181832df709692922e9042259fdbb7
Manaswi Humbe <manaswi1010@gmail.com>
2021-08-25 14:45:22 +0530

    Manaswi_Assignment20_Program_1to4

---
+++ src/manaswi/Assignment_20_Program2.java
@@ -0,0 +1,39 @@
+package manaswi;
+import java.util.Scanner;
+public class Assignment_20_Program2 
+{
+
+		private static Scanner sc;
+
+		void getFrequencyOfChar(String input, char ch) 
+		{
+			int count = 0;
+			for (int index = 0; index < input.length(); index++) 
+			{
+				if (input.charAt(index) == ch) 
+				{
+					count++;
+				}
+			}
+			System.out.println("In String " + input + " occurance of character " + ch + " is : " + count);
+		}
+
+		void getFrequencyOfEachChar(String input) 
+		{
+			for (int index = 0; index < input.length(); index++) 
+			{
+				char ch = input.charAt(index);
+				if (input.indexOf(ch) == index)
+					getFrequencyOfChar(input, ch);
+			}
+		}
+
+		public static void main(String[] args)
+		{
+			Assignment_20_Program2 program_2 = new Assignment_20_Program2();
+			sc = new Scanner(System.in);
+			System.out.println("Enter the String to which frequency of each character need to find ");
+			String word = sc.next();
+			program_2.getFrequencyOfEachChar(word);
+		}
+}
