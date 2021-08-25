276fdbe36201827e8e37866e7b35ad12c6ca9d26
devendrachandankhede <88403036+devendrachandankhede@users.noreply.github.com>
2021-08-25 18:18:36 +0530

    Update Assignment21_DiffBetweenOld_Young.java

--- src/devendra_Assignment_21/Assignment21_DiffBetweenOld_Young.java
+++ src/devendra_Assignment_21/Assignment21_DiffBetweenOld_Young.java
@@ -1,13 +1,9 @@
 package devendra_Assignment_21;
 
 //Find Age Difference Between Oldest And Youngest Family Members
-<<<<<<< Updated upstream
 
 public class Assignment21_DiffBetweenOld_Young {
-=======
-public class Assignment21_DiffBetweenOld_Young {
 
->>>>>>> Stashed changes
 	void printDiffBetweenOld_Young(int[] inputAge) {
 		int max = 0;
 		int min = inputAge[0];
@@ -16,18 +12,11 @@ public class Assignment21_DiffBetweenOld_Young {
 			if (max < inputAge[index])
 				max = inputAge[index];
 
-<<<<<<< Updated upstream
 			if (min > inputAge[index])
 				min = inputAge[index];
 		}
 
-		System.out.println("Difference Between Oldest & Youngest Family Member : " + (max - min));
-=======
-		if (min > inputAge[index])
-			min = inputAge[index];
-		}
 		System.out.println("Difference Between Oldest & Youngest Family Member Age : " + (max - min));
->>>>>>> Stashed changes
 	}
 
 	public static void main(String[] args) {
@@ -35,8 +24,4 @@ public class Assignment21_DiffBetweenOld_Young {
 		int[] inputAge = { 10, 34, 38, 68, 72, 95, 6 };
 		a21_OldYoung.printDiffBetweenOld_Young(inputAge);
 	}
-<<<<<<< Updated upstream
 }
-=======
-}
->>>>>>> Stashed changes
