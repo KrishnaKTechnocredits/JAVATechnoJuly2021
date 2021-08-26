f859fab9f1eb2ccb3ed1f97939f149c346127fea
Indu Bhartiya <indira.bhartiya@gmail.com>
2021-08-26 14:28:52 +0530

    Indu_Assignment_22

---
+++ src/indu/CharacterClass_A22_1.java
@@ -0,0 +1,44 @@
+package indu;
+
+import java.util.Scanner;
+
+public class CharacterClass_A22_1 {
+	
+	int i = 2;
+	int findTotalDigitInString(String str)
+	{
+		int count=0;
+		for (int index = 0;index<str.length();index++)
+		{
+			if(Character.isDigit(str.charAt(index)))
+			{
+				count++;
+			}
+		}
+		return count;
+	}
+	
+	int findSumofTotalDigitInString(String str)
+	{
+		int sum=0;
+		for (int index = 0;index<str.length();index++)
+		{
+			if(Character.isDigit(str.charAt(index)))
+			{
+				sum += Character.getNumericValue(str.charAt(index));
+			}
+		}
+		return sum;
+	}
+	
+	public static void main(String[] arf)
+	{
+		CharacterClass_A22_1 characterClass_A22_1 = new CharacterClass_A22_1();
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter Input String : ");
+		String str = scan.nextLine();
+		System.out.println("Total digits in given string is : " + characterClass_A22_1.findTotalDigitInString(str));
+		System.out.println("Total digits in given string is : " + characterClass_A22_1.findSumofTotalDigitInString(str));
+		
+	}
+}
