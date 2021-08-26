bfbf5581182905a4f2fc4098c43387a05f09a763
Chaitanya D <chaitanyadamarapakam@gmail.com>
2021-08-26 08:30:53 +0530

    ChaitanyaD_SurpriseCodingTest_1

--- src/chaitanya/FirstProgram.java
+++ src/chaitanya/FirstProgram.java
@@ -3,7 +3,7 @@ package chaitanya;
 public class FirstProgram {
 	
 	public static void main(String[] args) {
-		String name = "Chaitanya";
+		String name = "ChaitanyaD";
 		System.out.println(name);
 	}
 
