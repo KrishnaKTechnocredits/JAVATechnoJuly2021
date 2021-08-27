30a5591679e06dd6347e2e1d822c266454e0e777
NehaJoshi <neha19.a.joshi@gmail.com>
2021-08-27 13:28:49 +0530

    NehaJ_Package_Creation

---
+++ src/neha/assignment_13/Assignment13pg3.java
@@ -0,0 +1,24 @@
+package neha.assignment_13;
+/*program 3 : write a method which returns 5 smaller numbers of the given number.
+input : 50
+output : 49, 48, 47, 46, 45*/
+public class Assignment13pg3 {
+	int[] findFiveSmallerNumbers(int number) {
+		int[] output=new int[5];
+		for(int index=0;index<output.length;index++) {
+			number--;
+			output[index]=number;
+		}
+		return output;
+			}
+	public static void main(String[] args) {
+		Assignment13pg3 assignment13pg3=new Assignment13pg3();
+		int input=50;
+		int[] output=assignment13pg3.findFiveSmallerNumbers(input);
+		System.out.println("5 smaller numbers than input "+input+" are:");
+		for(int index=0;index<output.length;index++) {
+			System.out.print(output[index]+" ");
+		}
+	}
+
+}
