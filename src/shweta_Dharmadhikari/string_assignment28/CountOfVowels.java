d858159cdc5b0933c732ef90dfff5bd9463d9c1a
Shweta Dharmadhikari <shweta.dharmadhikari7@gmail.com>
2021-08-29 00:58:52 +0530

    ShwetaD_Assignment_24

--- src/shweta_Dharmadhikari/string_assignment28/CountOfVowels.java
+++ src/shweta_Dharmadhikari/string_assignment28/CountOfVowels.java
@@ -1,4 +1,5 @@
 package shweta_Dharmadhikari.string_assignment28;
+
 /*
  Assignment - 24 : 28th Aug'2021
 Program 1: Java Program to Count the Number of Vowels in user defined string.
@@ -6,45 +7,45 @@ Input : technocredits
 output : vowels are e , o, i
  */
 public class CountOfVowels {
-	int count=0;
-	
+	int count = 0;
+
 	void getVowels(String input) {
-		String vowel="";
-		for (int index = 0; index<input.length();index++) {
+		String vowel = "";
+		for (int index = 0; index < input.length(); index++) {
 			char ch = input.charAt(index);
 			if (index == input.indexOf(ch)) {
-				switch(ch)
-				{
+				switch (ch) {
 				case 'a':
 				case 'A':
-					vowel = vowel +"a";
+					vowel = vowel + "a";
 					count++;
 					break;
 				case 'e':
 				case 'E':
-					vowel = vowel +"e";
+					vowel = vowel + "e";
 					count++;
 					break;
 				case 'i':
 				case 'I':
-					vowel = vowel +"i";
+					vowel = vowel + "i";
 					count++;
 					break;
 				case 'o':
 				case 'O':
-					vowel = vowel +"o";
+					vowel = vowel + "o";
 					count++;
 					break;
 				case 'u':
 				case 'U':
-					vowel = vowel +"o";
+					vowel = vowel + "o";
 					count++;
-					break;	
+					break;
 				}
 			}
-		} 
-		System.out.println("Count of Vowels -> "+count+ "\nVowels are : "+vowel);
+		}
+		System.out.println("Count of Vowels -> " + count + "\nVowels are : " + vowel);
 	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		CountOfVowels countOfVowels = new CountOfVowels();
