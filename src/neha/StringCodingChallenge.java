ed5b0cdd4b4a123e972bd0d4d215110a1092dce9
NehaJoshi <neha19.a.joshi@gmail.com>
2021-08-29 12:58:42 +0530

    NehaJ_SCoding_Challenge_Refactoring

--- src/neha/StringCodingChallenge.java
+++ src/neha/StringCodingChallenge.java
@@ -1,34 +1,32 @@
 package neha;
 
-import java.util.Arrays;
-
 /*String str = "Ma1U2liK"
 output : MUK12ali*/
 public class StringCodingChallenge {
-	String[] findCapitalSmallLettersNumbers(String str) {
-		String[] output = { "", "", "" };
-
+	String findCapitalSmallLettersNumbers(String str) {
+		String capitalStr = "";
+		String smallStr = "";
+		String numStr = "";
 		for (int index = 0; index < str.length(); index++) {
 
 			char ch = str.charAt(index);
-			if (ch >= 'a' && ch <= 'z')
-				output[0] = String.valueOf(ch) + output[0];
 			if (ch >= 'A' && ch <= 'Z')
-				output[1] = String.valueOf(ch) + output[1];
+				capitalStr = capitalStr + String.valueOf(ch);
+			if (ch >= 'a' && ch <= 'z')
+				smallStr = smallStr + String.valueOf(ch);
 			if (Character.isDigit(ch)) {
-				output[2] = output[2] + Character.getNumericValue(ch);
+				numStr = numStr + Character.getNumericValue(ch);
 			}
 
 		}
-		return output;
+		return capitalStr + numStr + smallStr;
 
 	}
 
 	public static void main(String[] args) {
 		StringCodingChallenge stringCodingChallenge = new StringCodingChallenge();
 		String str = "Ma1U2liK";
-		System.out.println("Capital, Small letters and numbers are separated and collected in array as follows: "
-				+ Arrays.toString(stringCodingChallenge.findCapitalSmallLettersNumbers(str)));
+		System.out.println("Output is: " + stringCodingChallenge.findCapitalSmallLettersNumbers(str));
 	}
 
 }
