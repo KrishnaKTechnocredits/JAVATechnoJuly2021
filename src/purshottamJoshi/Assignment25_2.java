51424bcafeaae7a5c4ab3d60c0f382df357ba346
purshottam94 <71837062+purshottam94@users.noreply.github.com>
2021-08-29 22:39:18 +0530

    Update Assignment25_2.java

--- src/purshottamJoshi/Assignment25_2.java
+++ src/purshottamJoshi/Assignment25_2.java
@@ -7,12 +7,12 @@ class Assignment25_2{
 		for (int index = 0; index < newstr.length(); index++) {
 			if (Character.isDigit(newstr.charAt(index))) {
 				str += newstr.charAt(index);
-				System.out.println(": "+str);
+				//System.out.println(": "+str);
 			}else if (!str.equals("")){
 				sum += Integer.parseInt(str);
 				str = "";
 			}
-			}
+		}
 		return sum;
 	}
 	public static void main(String[] args){
