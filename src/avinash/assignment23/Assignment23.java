0bc03591954fdbf76ae0ae727e21892ead4c9f85
Avinash Patil <avinashspatil12@gmail.com>
2021-08-29 23:33:12 +0530

    Avinash_Assignment_23_modified_as_per_the_Comment

--- src/avinash/assignment23/Assignment23.java
+++ src/avinash/assignment23/Assignment23.java
@@ -1,104 +1,124 @@
 package avinash.assignment23;
 
 public class Assignment23 {
-	int totalBalance;
-	int intialICICIBal,initialHDFCBal,initialBOBBal,intialSBIBal;
-	static String username;
-	static int totalCreditCnt;
-	static int totalDebitCnt;
-	
-	void setUserDetails(String customerName,int ICICIBal,int HDFCBal,int BOBBal,int SBIBal) {
-		username = customerName;
-		intialICICIBal = ICICIBal;
-		initialHDFCBal = HDFCBal;
-		initialBOBBal = BOBBal;
-		intialSBIBal = SBIBal;		
-	}
-	
-	int printTotalBalance(){
-		
-		totalBalance = intialICICIBal + initialHDFCBal + initialBOBBal + intialSBIBal;
-		return totalBalance;	
-	}
-	
-	void individualCreditBal(String bankName,int creditCount) {
-		switch(bankName) {
+	int iciciAmount = 4000;
+	int hdfcAmount = 5000;
+	int bobAmount = 6000;
+	int sbiAmount = 9000;
+	int iciciDebitCount, hdfcDebitCount, bobDebitCount, sbiDebitCount;
+	int iciciCreditCount, hdfcCreditCount, bobCreditCount, sbiCreditCount;
+
+	void bankOperations(String bankName, String operation, int amount) {
+		switch (bankName) {
 		case "ICICI":
-			System.out.println("ICICI bank Credit Operation - " + creditCount);
+			if (operation.equalsIgnoreCase("Debit")) {
+				if (iciciAmount > amount) {
+					iciciAmount = iciciAmount - amount;
+					iciciDebitCount++;
+				} else
+					System.out.println("Inadequate ICICI account balance.");
+			} else {
+				iciciAmount = iciciAmount + amount;
+				iciciCreditCount++;
+			}
 			break;
 
 		case "HDFC":
-			System.out.println("HDFC bank Credit Operation - " + creditCount);
-			break;
+			if (operation.equalsIgnoreCase("Debit")) {
+				if (hdfcAmount > amount) {
+					hdfcAmount = hdfcAmount - amount;
+					hdfcDebitCount++;
+				} else
+					System.out.println("Inadequate HDFC account balance.");
+			} else {
+				hdfcAmount = hdfcAmount + amount;
+				hdfcCreditCount++;
+			}
 
+			break;
 		case "BOB":
-			System.out.println("BOB bank Credit Operation - " + creditCount);
+			if (operation.equalsIgnoreCase("Debit")) {
+				if (hdfcAmount > amount) {
+					bobAmount = bobAmount - amount;
+					bobDebitCount++;
+				} else
+					System.out.println("Inadequate BOB account balance.");
+			} else {
+				bobAmount = bobAmount + amount;
+				bobCreditCount++;
+			}
+
 			break;
 
 		case "SBI":
-			System.out.println("SBI bank Credit Operation - " + creditCount);
+			if (operation.equalsIgnoreCase("Debit")) {
+				if (sbiAmount > amount) {
+					sbiAmount = sbiAmount - amount;
+					sbiDebitCount++;
+				} else
+					System.out.println("Inadequate SBI account balance.");
+			} else {
+				sbiAmount = sbiAmount + amount;
+				sbiCreditCount++;
+			}
 			break;
+
 		default:
-			
+			System.out.println("Invalid bank name");
 		}
-		totalCreditCnt +=creditCount;
-	}
-	
-	
-	
-	void individualDebitBal(String bankName,int debitCount) {
-		switch(bankName) {
-		case "ICICI":
-			System.out.println("ICICI bank Debit Operation - " + debitCount);
-			break;
-
-		case "HDFC":
-			System.out.println("HDFC bank Debit Operation - " + debitCount);
-			break;
-
-		case "BOB":
-			System.out.println("BOB bank Debit Operation - " + debitCount);
-			break;
-
-		case "SBI":
-			System.out.println("SBI bank Debit Operation - " + debitCount);
-			break;
-		default:
-			
-		}
-		totalDebitCnt +=debitCount;
-	}
-	
-	void totalBankCredDebitCnt() {
-		System.out.println("Total Credit operation - " + totalCreditCnt + " times");
-		System.out.println("Total Debit operation - " + totalDebitCnt + " times");
 
 	}
 
+	void displayTotalBal() {
+		int totalBal = iciciAmount + hdfcAmount + bobAmount + sbiAmount;
+		System.out.println("The total account balance across 4 banks is Rs." + totalBal);
+	}
+
+	void displayTotalCredit() {
+
+		int totalCredit = iciciCreditCount + hdfcCreditCount + bobCreditCount + sbiCreditCount;
+		System.out.println("Overall count of credit operations across 4 banks is " + totalCredit);
+
+	}
+
+	void displayTotalDebit() {
+
+		int totalDebit = iciciDebitCount + hdfcDebitCount + bobDebitCount + sbiDebitCount;
+		System.out.println("Overall count of debit operations acoss 4 banks is " + totalDebit);
+	}
+
+	void displayIndCredOpt() {
+		System.out.println("Overall count of credit operations on ICICI bank account is " + iciciCreditCount);
+		System.out.println("Overall count of credit operations on HDFC bank account is " + hdfcCreditCount);
+		System.out.println("Overall count of credit operations on BOB bank account is " + bobCreditCount);
+		System.out.println("Overall count of credit operations on SBI bank account is " + sbiCreditCount);
+	}
+
+	void displayIndDebitOpt() {
+		System.out.println("Overall count of debit operations on ICICI bank account is " + iciciDebitCount);
+		System.out.println("Overall count of debit operations on HDFC bank account is " + hdfcDebitCount);
+		System.out.println("Overall count of debit operations on BOB bank account is " + bobDebitCount);
+		System.out.println("Overall count of debit operations on SBI bank account is " + sbiDebitCount);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Assignment23 assignment23 = new Assignment23();
-		assignment23.setUserDetails("Chad", 8000, 5000, 9000, 6000);
-		System.out.println(username + " all transaction summary ->");
-		System.out.println("Show Total balance -:");
-		System.out.println(assignment23.printTotalBalance());
-		System.out.println("----------------------------------------");
-
-		System.out.println("Show individual bank credit operations -:");
-		assignment23.individualCreditBal("ICICI", 2);
-		assignment23.individualCreditBal("HDFC", 2);
-		assignment23.individualCreditBal("SBI", 1);
-		assignment23.individualCreditBal("BOB", 4);
-		assignment23.individualCreditBal("SBI", 4);
-		System.out.println("----------------------------------------");
-		System.out.println("Show individual bank debit operations -:");
-		assignment23.individualDebitBal("ICICI", 2);
-		assignment23.individualDebitBal("HDFC", 2);
-		assignment23.individualDebitBal("BOB", 2);
-		assignment23.individualDebitBal("SBI", 2);
-		System.out.println("----------------------------------------");
-		assignment23.totalBankCredDebitCnt();
+		assignment23.bankOperations("ICICI", "Debit", 1000);
+		assignment23.bankOperations("ICICI", "Credit", 500);
+		assignment23.bankOperations("HDFC", "Debit", 750);
+		assignment23.bankOperations("HDFC", "Credit", 1000);
+		assignment23.bankOperations("SBI", "Debit", 2500);
+		assignment23.bankOperations("SBI", "Credit", 500);
+		assignment23.bankOperations("BOB", "Credit", 525);
+		assignment23.bankOperations("BOB", "Debit", 950);
+		assignment23.bankOperations("ICICI", "Debit", 200);
+		assignment23.bankOperations("HDFC", "Credit", 750);
+		assignment23.displayTotalBal();
+		assignment23.displayTotalCredit();
+		assignment23.displayIndCredOpt();
+		assignment23.displayTotalDebit();
+		assignment23.displayIndDebitOpt();
 
 	}
 
