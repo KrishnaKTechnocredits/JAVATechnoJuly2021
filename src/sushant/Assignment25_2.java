c4656e719e8dbbf162f0ff221033915216688f09
Sushant Sonawane <sonawane.sushant87@gmail.com>
2021-08-30 15:56:33 +0530

    sushant_Assignment25

---
+++ src/sushant/Assignment25_2.java
@@ -0,0 +1,31 @@
+/*Program 1:
+String str = "te12ch33no3credit4s"; 
+output = 12+33+3+4
+
+Program2:
+String str = "Ma1U2liK"
+output : MUK12ali*/
+
+package sushant;
+
+class Assignment25_2{
+	int getSumOfNumber(String newstr) {
+		int sum = 0;
+		String str = "";
+		for (int index = 0; index < newstr.length(); index++) {
+			if (Character.isDigit(newstr.charAt(index))) {
+				str += newstr.charAt(index);
+				//System.out.println(": "+str);
+			}else if (!str.equals("")){
+				sum += Integer.parseInt(str);
+				str = "";
+			}
+		}
+		return sum;
+	}
+	public static void main(String[] args){
+		Assignment25_2 assignment25_2 = new Assignment25_2();
+		String str ="te12ch33no3credit4s";
+		System.out.println("sum of numbers in string : "+assignment25_2.getSumOfNumber(str));
+	}
+}
