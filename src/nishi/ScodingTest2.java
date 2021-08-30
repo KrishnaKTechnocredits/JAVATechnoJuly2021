6fff82917fa551ec9da5eeee2cb77a3750dc2f37
Nishi Bharti <iamnishibharti@gmail.com>
2021-08-30 09:05:02 +0530

    Nishi_Scoding_Test2

---
+++ src/nishi/ScodingTest2.java
@@ -0,0 +1,32 @@
+package nishi;
+
+public class ScodingTest2 {
+	String getOutput(String input)
+	{		
+		String str1="";
+		int num=0;
+		
+		for(int i=0;i<input.length();i++)
+		{
+			if(Character.isUpperCase(input.charAt(i)))
+			{
+				str1+=(String.valueOf(input.charAt(i))).toLowerCase();				
+			}
+			else if(Character.isDigit(input.charAt(i)))
+			{
+				str1+=input.charAt(i);
+			}
+			else if(Character.isLowerCase(input.charAt(i)))
+			{
+				str1+=(String.valueOf(input.charAt(i))).toUpperCase();	
+			}
+		}
+		return str1;
+	}	
+
+	public static void main(String[] args) {
+			String input = "A1ashVi6";
+			System.out.println("The output : "+new ScodingTest2().getOutput(input));
+	}
+
+}
