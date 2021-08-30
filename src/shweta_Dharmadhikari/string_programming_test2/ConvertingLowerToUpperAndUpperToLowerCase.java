5e563517f4d6f0c175a4f891fa59619495cc1a24
Shweta Dharmadhikari <shweta.dharmadhikari7@gmail.com>
2021-08-30 10:01:15 +0530

    ShwetaD_ProgrammingTest2

---
+++ src/shweta_Dharmadhikari/string_programming_test2/ConvertingLowerToUpperAndUpperToLowerCase.java
@@ -0,0 +1,37 @@
+package shweta_Dharmadhikari.string_programming_test2;
+
+/*
+ * Programming Test - 2 :
+
+Convert all lowercase to uppercase and uppercase to lowercase in a given string.
+
+Input : A1ashVi6
+Output : a1ASHvI6
+ */
+public class ConvertingLowerToUpperAndUpperToLowerCase {
+
+	String output = "";
+
+	String convertingLetterCaseFromString(String str) {
+		char ch[] = str.toCharArray();
+		for (int index = 0; index < ch.length; index++) {
+			if (Character.isUpperCase(ch[index])) {
+				ch[index] = Character.toLowerCase(ch[index]);
+				output += ch[index];
+			} else if (Character.isLowerCase(ch[index])) {
+				ch[index] = Character.toUpperCase(ch[index]);
+				output += ch[index];
+			} else
+				output += ch[index];
+		}
+		return output;
+	}
+
+	public static void main(String[] args) {
+		String str = "A1ashVi6";
+		ConvertingLowerToUpperAndUpperToLowerCase convertingLowerToUpperAndUpperToLowerCase = new ConvertingLowerToUpperAndUpperToLowerCase();
+		String output = convertingLowerToUpperAndUpperToLowerCase.convertingLetterCaseFromString(str);
+		System.out.println(output);
+	}
+
+}
