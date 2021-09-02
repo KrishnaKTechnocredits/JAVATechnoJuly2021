58f2955ad7f513ca76d41019b7e159e035f057c1
Manaswi Humbe <manaswi1010@gmail.com>
2021-09-02 09:26:26 +0530

    Manaswi_Assignment_26_Program_1to4

---
+++ src/manaswi/SwapTwoNumbersWithoutThirdVariable.java
@@ -0,0 +1,19 @@
+package manaswi;
+public class SwapTwoNumbersWithoutThirdVariable 
+{
+
+	void swapTwoNumbers(int a,int b)
+	{
+		System.out.println("Number a : "+a+"\nNumber b : "+b);
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println("After swapping : \na : "+a+"\nb : "+b);
+	}
+	public static void main(String[] args)
+	{
+		SwapTwoNumbersWithoutThirdVariable swapTwoNumbersWithoutThirdVariable=new SwapTwoNumbersWithoutThirdVariable();
+		swapTwoNumbersWithoutThirdVariable.swapTwoNumbers(15,45);
+	}
+
+}
