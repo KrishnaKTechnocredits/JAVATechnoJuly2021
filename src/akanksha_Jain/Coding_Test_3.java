24db17316b25d5c38f3284d40598e4b6c39b7b98
Akansha Jain <goyanka.akku@gmail.com>
2021-09-03 08:42:01 +0530

    AkanshaJain_CodingTest_3

---
+++ src/akanksha_Jain/Coding_Test_3.java
@@ -0,0 +1,23 @@
+package akanksha_Jain;
+
+import java.util.Arrays;
+
+public class Coding_Test_3 {
+
+	void removeNumberFromArray(int[] input, int num) {
+		int[] output = new int[input.length-1];
+		int outputIndex=0;
+		for(int index=0; index<input.length; index++) {
+			if(input[index] != num)
+				output[outputIndex++] = input[index];
+		}
+		System.out.println("Output:- " + Arrays.toString(output));
+	}
+	
+	public static void main(String[] args) {
+		int input[] = {25, 14, 56, 15, 36, 56, 77, 18, 29, 49};
+		int num = 14;
+		Coding_Test_3 test = new Coding_Test_3();
+		test.removeNumberFromArray(input, num);
+	}
+}
