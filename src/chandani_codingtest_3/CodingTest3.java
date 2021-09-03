73850a6423407b7cab44f93d07f69a91897a88a9
Chandni Fatehchandani <chandnif6@gmail.com>
2021-09-03 10:36:26 +0530

    Chandani_codingtest_3_corrections

--- src/chandani_codingtest_3/CodingTest3.java
+++ src/chandani_codingtest_3/CodingTest3.java
@@ -4,22 +4,25 @@ import java.util.Arrays;
 
 public class CodingTest3{
 
-		int[] getArrayAfterRemovingANumber(int[] input){
-			int output[] = new int[input.length-1];
-			output[0] = input[0];
-			
-			for(int index = 1 ; index < input.length-1; index++){
-				output[index] = input[(index+1)];
+	int[] getArrayAfterRemovingANumber(int[] input, int num){
+		int output[] = new int[input.length-1];
+		int count = 0;
+		for(int index = 0 ; index < input.length; index++) {
+				if(input[index]!= num) {
+					output[count] = input[(index)];
+					count++;
+				}
 			}
-			return output;
+		return output;
 		}	
-			
-	
-		public static void main(String[] args){
-			CodingTest3 codingTest3 = new CodingTest3();	
-			int[] arr = {25,14,56,15,36,56,77,18,29,49};
-			System.out.println("After removing " + arr[1] + " from the given array  "
-			+ Arrays.toString(arr) + " the output is : "  + Arrays.toString(codingTest3.getArrayAfterRemovingANumber(arr)));
+		
 
-		}	
+	public static void main(String[] args){
+		CodingTest3 codingTest3 = new CodingTest3();	
+		int[] arr = {25,14,56,15,36,56,77,18,29,49};
+		int num = 14;
+		System.out.println("After removing " + num + " from the given array  "
+		+ Arrays.toString(arr) + " the output is : "  + Arrays.toString(codingTest3.getArrayAfterRemovingANumber(arr, num)));
+
+	}	
 }
