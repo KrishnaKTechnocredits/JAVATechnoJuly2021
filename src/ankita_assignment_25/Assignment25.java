5f2345a812defec5c9748c8862aceb1d475e6f2f
Ankita Kene <ankitakene44@gmail.com>
2021-09-03 13:05:58 +0530

    ankita_assignment_25

---
+++ src/ankita_assignment_25/Assignment25.java
@@ -0,0 +1,25 @@
+package ankita_assignment_25;
+
+public class Assignment25 {
+	int sum = 0;
+	String input = "";
+
+	void sumNumbers(String str) {
+		for (int index = 0; index < str.length(); index++) {
+			if (Character.isDigit(str.charAt(index)))
+				input += str.charAt(index);
+			else if (input != "") {
+				sum += Integer.parseInt(input);
+				input = "";
+			}
+		}
+		System.out.println("Output:" + sum);
+
+	}
+
+	public static void main(String[] args) {
+		Assignment25 assignment25 = new Assignment25();
+		assignment25.sumNumbers("te12ch33no3credit4s");
+
+	}
+}
