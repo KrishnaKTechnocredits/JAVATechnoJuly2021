68994875dbe564fbc6836c8ece8ab0f12a39dcff
Manaswi Humbe <manaswi1010@gmail.com>
2021-09-05 00:24:25 +0530

    Manaswi_Assignment_28_Program_1and2

---
+++ src/manaswi/SecondHighestNumber.java
@@ -0,0 +1,37 @@
+package manaswi;
+public class SecondHighestNumber
+ {
+	int getSecondHighestNumber(int[] arr)
+	{
+		int highestNumber = 0;
+		int secondHighestNumber = 0;
+		if(arr[0] > arr[1]) 
+		{
+			highestNumber = arr[0];
+			secondHighestNumber = arr[1];
+		}
+		for(int index = 3; index < arr.length; index++)
+		{
+			if(arr[index] > highestNumber) 
+			{
+				secondHighestNumber = highestNumber;
+				highestNumber = arr[index];
+			}
+			else if(arr[index] > secondHighestNumber)
+			{
+				secondHighestNumber = arr[index];
+			}		
+		}	
+		return secondHighestNumber;
+	}
+	void displaySecondHighestNumber(int[] arr)
+	{
+		System.out.println("Second highest number from the array is: "+getSecondHighestNumber(arr));
+	}
+	public static void main(String[] args) 
+	{
+		SecondHighestNumber secondHighestNumber = new SecondHighestNumber();
+		int[] arr = {10,23,2,11,55,43,99};
+		secondHighestNumber.displaySecondHighestNumber(arr);
+	}
+}
