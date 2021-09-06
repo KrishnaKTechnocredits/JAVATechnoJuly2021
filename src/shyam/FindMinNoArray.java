c495a750c34cb756002d09b4539fd3c532923b1a
Shyam Anasane <shyamanasane@gmail.com>
2021-09-06 21:11:24 +0530

    Shyam Assignment 14

---
+++ src/shyam/FindMinNoArray.java
@@ -0,0 +1,22 @@
+package shyam;
+
+public class FindMinNoArray {
+	
+	int minNumber;
+	public static void main(String[] args) {
+		int[] inputArray = {125,11,43,55,99,23,5};
+		FindMinNoArray findMinNumber = new FindMinNoArray();
+		findMinNumber.findMinNumberInArray(inputArray);
+		System.out.println("Minimum number found in the given array is : "+findMinNumber.minNumber);
+	}
+
+	int findMinNumberInArray(int[] arrayInput) {
+		minNumber = arrayInput[0];
+		for(int index = 0 ; index < arrayInput.length; index++) {
+				if(minNumber > arrayInput[index])
+					minNumber = arrayInput[index];
+		}
+		return minNumber;
+	}
+
+}
