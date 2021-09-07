b8724ac5ce430df60983e50a775594278dfdfe86
Akansha Jain <goyanka.akku@gmail.com>
2021-09-07 23:34:51 +0530

    AkanshaJain Assignment 29

---
+++ src/akanksha_Jain/Assignment_29/Program_2.java
@@ -0,0 +1,48 @@
+package akanksha_Jain.Assignment_29;
+
+public class Program_2 {
+
+	boolean isPallindrome(String input) {
+		int count = input.length();
+		int middleIndex = count/2;
+		boolean flag = true;
+		if(middleIndex/2==0) {
+			for (int index=0; index<=middleIndex-1; index++) {
+				if(input.charAt(index)!=input.charAt(count-1)) {
+					flag = false;
+					break;
+				}
+				else 
+					count --;
+			}
+		}
+		else {
+			for (int index=0; index<middleIndex; index++) {
+				if(input.charAt(index)!=input.charAt(count-1)) {
+					flag = false;
+					break;
+				}
+				else 
+					count --;
+			}
+		}
+		return flag;
+	}
+	
+	void displayPallindromeResult(String input) {
+		if(isPallindrome(input))
+			System.out.println(input + " is pallindrome string.");
+		else
+			System.out.println(input + " is not a pallindrome string.");
+	}
+	
+	public static void main(String[] args) {
+		String input = "malayalam";
+		String input2 = "aabbaa";
+		String input3 = "akansha";
+		Program_2 program_2 = new Program_2();
+		program_2.displayPallindromeResult(input);
+		program_2.displayPallindromeResult(input2);
+		program_2.displayPallindromeResult(input3);
+	}
+}
