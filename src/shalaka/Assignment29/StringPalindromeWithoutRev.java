71759e0a114e97c61e86dc82efaa049ee7ab1fd2
Shalaka Prayag <shalaka159@gmail.com>
2021-09-07 18:33:57 +0530

    Shalaka_Assignment29

---
+++ src/shalaka/Assignment29/StringPalindromeWithoutRev.java
@@ -0,0 +1,27 @@
+/*program 2: String Pallindrome by without writing reverse method logic.
+ String input = "naman";
+ String input = "aabbaa";*/
+package shalaka.Assignment29;
+
+public class StringPalindromeWithoutRev {
+	boolean isPalindrome=false;
+	boolean isPalindrome(String input) {
+		for(int index=0;index<input.length();index++) {
+			if (index>=(input.length()-index))
+				break;
+			if(input.charAt(index)==input.charAt(input.length()-1-index))
+				isPalindrome=true;	
+			else
+				isPalindrome=false;	
+		}
+		return isPalindrome;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 String input = "aabbaa";
+		 StringPalindromeWithoutRev stringPalindromeWithoutRev =new StringPalindromeWithoutRev();
+		 boolean isPalindrome=stringPalindromeWithoutRev.isPalindrome(input);
+		 System.out.println(isPalindrome);
+	}
+
+}
