4a4ff822dc436070c00f5f4fdca09aa5c1ee7474
Margi Santoki <margisantoki@gmail.com>
2021-09-09 15:42:08 +0530

    Margi_CodingTest_5

---
+++ src/margi/CodingTest/CodingTest_5.java
@@ -0,0 +1,26 @@
+package margi.CodingTest;
+
+public class CodingTest_5 {
+
+	void getPerfectNumbers() {
+		int count=0;
+		long sum, number = 1;
+		while(count<=5) {
+			sum=0;
+			for(int index=1; index<number; index++) {
+				if(number%index==0)
+					sum += index;
+			}
+			if(number == sum) {
+				System.out.println(number);
+				count++;
+			}
+			number++;
+		}
+	}
+	
+	public static void main(String[] args) {
+		CodingTest_5 codingTest_5 = new CodingTest_5();
+		codingTest_5.getPerfectNumbers();
+	}
+}
