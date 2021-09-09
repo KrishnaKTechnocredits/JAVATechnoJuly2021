6e542835711a6cf71014f91ab54f3c737fc02c9b
Parthav Dani <partharjun@gmail.com>
2021-09-09 16:57:27 -0400

    ParthavD_Codingtest_4 #13mins

---
+++ src/parthav/ShiftZerosInArray.java
@@ -0,0 +1,47 @@
+//Test 4 : Shift all Zero's at the end of the array. 
+//int[] arr = {1,0,0,6,0,2,3,0,4,0,5};
+//output : {1,6,2,3,4,5,0,0,0,0,0};
+
+package parthav;
+
+public class ShiftZerosInArray {
+
+	int[] getArrayWithZerosShifted(int[] inputArr) {
+		int tempCountOfZeros = 0;
+		int[] returnedArr = new int[inputArr.length];
+		int returnedArrIndex = 0;
+		for (int index = 0; index < inputArr.length; index++) {
+
+			if (inputArr[index] != 0) {
+				returnedArr[returnedArrIndex] = inputArr[index];
+				returnedArrIndex++;
+			} else
+				tempCountOfZeros++;
+
+		}
+		returnedArrIndex = 0;
+		while (tempCountOfZeros > 0) {
+			if (returnedArr[returnedArrIndex] < 1) {
+				returnedArr[returnedArrIndex] = 0;
+				tempCountOfZeros--;
+			}
+
+			returnedArrIndex++;
+
+		}
+
+		return returnedArr;
+	}
+
+	public static void main(String[] args) {
+		ShiftZerosInArray newObject = new ShiftZerosInArray();
+		int[] inputArr = { 1, 0, 0, 6, 0, 2, 3, 0, 4, 0, 5 };
+		int[] returedArr = newObject.getArrayWithZerosShifted(inputArr);
+		System.out.print("{");
+		for (int index = 0; index < returedArr.length; index++) {
+			System.out.print(returedArr[index] + ",");
+		}
+		System.out.print("}");
+	}
+
+}
