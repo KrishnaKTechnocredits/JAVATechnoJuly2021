add7e43453f5f3c325f40921f59b28305df24d05
Santosh Ghige <santoshghige1993@gmail.com>
2021-09-10 22:39:51 +0530

    Santosh_CodingTest_5

---
+++ src/santosh/CodingTest_5.java
@@ -0,0 +1,31 @@
+package santosh;
+public class CodingTest_5 {
+	boolean isPerfectNumber(long input) {
+		long output = 0;
+		for (long index = 1; index < input; index++) {
+			if (input % index == 0) {
+				output += index;
+			}
+		}
+		if (output == input)
+			return true;
+		return false;
+	}
+	void printFirstNPerfectNumbers(int n) {
+		int count = 0;
+		long perfectNumber = 1;
+		while (count <= 5) {
+			if (isPerfectNumber(perfectNumber)) {
+				count++;
+				System.out.println(perfectNumber);
+			}
+			perfectNumber++;
+		}
+	}
+	public static void main(String[] args) {
+		CodingTest_5 test5= new CodingTest_5();
+		System.out.println("first 5 perfect no.");
+        test5.printFirstNPerfectNumbers(5);
+		
+	}
+}
