682f2e6467257ed6698f2de6007fba62b8acd508
Monika Zankar <monikazankar@gmail.com>
2021-09-11 11:11:07 -0700

    Updateed

---
+++ src/monika.Assignment_30/PerfecrSquare.java
@@ -0,0 +1,27 @@
+package Assignment_30;
+
+public class PerfecrSquare {
+
+	boolean isperfectSquare(int number) {
+		boolean flag=false;
+		for(int index=1;index<=number/2;index++) {
+			
+			if(index*index==number)
+				flag= true;
+		}
+		return flag;
+		
+	}
+	
+	void displayResult(int num) {
+		if(isperfectSquare(num))
+			System.out.println(num+" is perfect square");
+		else
+			System.out.println(num+" is not perfect square");
+	}
+	
+	public static void main(String[] args) {
+		PerfecrSquare perfcetNo=new PerfecrSquare();
+		perfcetNo.displayResult(25);
+	}
+}
---
+++ src/monika.Assignment_30/ReverseNumber.java
@@ -0,0 +1,24 @@
+/*Assignment - 30 : 8th Sep'2021
+Program 1: Write a method to reverse a given number.
+input : 1932
+output : 2391*/
+
+package Assignment_30;
+
+public class ReverseNumber {
+	void reverseNumber(int number)
+	{
+		int reverse=0, remainder;
+		System.out.println("Given Number is: "+number);
+		for(;number!=0;number=number/10) {
+			remainder=number%10;
+			reverse=reverse * 10 + remainder;
+		}
+		
+		System.out.println("Reverse Number is: "+reverse);
+	}
+	public static void main(String[] args) {
+		ReverseNumber reversenumber=new ReverseNumber();
+		reversenumber.reverseNumber(12345);
+	}
+}
