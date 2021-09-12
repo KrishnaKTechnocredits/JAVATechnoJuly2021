034cb8addb3cde4bad67a9d53a64f1807481bcf2
Siddharth Tambe <tsiddharth.463@gmail.com>
2021-09-12 20:23:08 +0530

    SiddharthT_assignment_30

---
+++ src/siddharth/Assignment30_2.java
@@ -0,0 +1,24 @@
+package siddharth;
+
+public class Assignment30_2 {
+	void armstrongNumber(int num) {
+		int sum = 0;
+		int r = 0;
+		int num1 = num;
+		while (num1 > 0) {
+			r = num1 % 10;
+			sum += (r * r * r);
+			num1 = num1 / 10;
+		}
+		if (num == sum)
+			System.out.println(num + " is an armstrong number");
+		else
+			System.out.println(num + " is not an armstrong number");
+	}
+
+	public static void main(String[] args) {
+		Assignment30_2 assignment30_2 = new Assignment30_2();
+		int num = 153;
+		assignment30_2.armstrongNumber(num);
+	}
+}
---
+++ src/siddharth/Assignment30_3.java
@@ -0,0 +1,25 @@
+package siddharth;
+
+public class Assignment30_3 {
+	void isPalindromeNumber(int num) {
+		int num1 = num;
+		int rnum = 0;
+		while (num1 > 0) {
+			int r = num1 % 10;
+			rnum = rnum * 10 + r;
+			num1 = num1 / 10;
+		}
+		if (rnum == num)
+			System.out.println(num + " is a palindrome number");
+		else
+			System.out.println(num + " is not a palindrome number");
+	}
+
+	public static void main(String[] args) {
+		Assignment30_3 assignment30_3 = new Assignment30_3();
+		int num1 = 12321;
+		int num2 = 123;
+		assignment30_3.isPalindromeNumber(num1);
+		assignment30_3.isPalindromeNumber(num2);
+	}
+}
