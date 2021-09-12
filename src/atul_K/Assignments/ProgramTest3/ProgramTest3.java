36e7c7772c9205e5df6558711eccdbe7f84a40e3
102522 <atul.0101@gmail.com>
2021-09-12 20:16:51 +0530

    Atul_Ass32_Test3_6

--- src/atul_K/Assignments/ProgramTest3/ProgramTest3.java
+++ src/atul_K/Assignments/ProgramTest3/ProgramTest3.java
@@ -9,4 +9,24 @@ package atul_K.Assignments.ProgramTest3;
 
 public class ProgramTest3 {
 
+	void removeElementFromArray(int[] arr, int num) {
+		int[] out = new int[arr.length-1];
+		int i = 0;
+		for(int index=0; index < arr.length;index++) {
+			if (num != arr[index]) {
+				out[i] = arr[index];
+				i++;
+			}		
+		}
+		for(int index=0;index<out.length;index++) {
+			System.out.print(out[index]+" ");
+		}
+	}
+	
+	public static void main(String[] args) {
+		int input[] = {25, 14, 56, 15, 36, 56, 77, 18, 29, 49};
+		ProgramTest3 p3 = new ProgramTest3();
+		p3.removeElementFromArray(input, 14);
+	}
 }
+//time : 09 min
