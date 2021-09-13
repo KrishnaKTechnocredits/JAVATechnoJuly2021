e018e77f654cbeea2b45669817d69df74dc8afa4
Sushant Sonawane <sonawane.sushant87@gmail.com>
2021-09-13 21:40:15 +0530

    sushant_codingTest6

---
+++ src/sushant/CodingTest_6.java
@@ -0,0 +1,53 @@
+/*Program 1: Find first non repeating character from a word. [expected : 10 mins]
+input : technocredits
+output : h 
+
+Program 2: Find first non repeating word from a given String. [expected : 15 mins]
+input : hi this is techno gm credits this hi is 
+output : techno*/
+
+package sushant;
+
+import java.util.Scanner;
+
+public class CodingTest_6 {
+
+	char firstNonRepeatingChar(String input) {
+		char ch=' ';
+		for(int i=0;i<input.length();i++)
+			if(i==input.lastIndexOf(input.charAt(i))) {
+				ch=input.charAt(i);
+				break;
+			}
+		return ch;
+	}
+
+	String firstNonRepeatingWord(String[] input) {
+		int i,j,count;
+		for (i=0;i<input.length;i++) {
+			count=0;
+			for (j=0;j<input.length;j++) {
+				if(input[i].equals(input[j]) )
+					count++;
+			}
+			if(count==1)
+				break;
+		}
+		return input[i];
+	}
+	public static void main(String[] args) {
+		CodingTest_6 codingTest_6 = new CodingTest_6();
+		Scanner scan =new Scanner(System.in);
+		System.out.println("Enter the Word to find first non repeating character: ");
+		String input=scan.nextLine();
+		System.out.println( "First non repeating character of "+input+" is "+codingTest_6.firstNonRepeatingChar(input));
+
+		System.out.println("Enter the String : ");
+		String[] arr = scan.nextLine().split(" ");
+		System.out.println( "First non repeating word is "+codingTest_6.firstNonRepeatingWord(arr));
+
+		scan.close();
+
+	}
+
+}
