7852a5f38fd853cb87e786769d5fab217e1032d3
DATTARAJ WETE <dattarajwete@gmail.com>
2021-09-14 17:27:23 +0530

    Dattaraj Scoding Test 6 Program 2

--- src/dattaraj/Scoding_Test6.java
+++ src/dattaraj/Scoding_Test6.java
@@ -5,10 +5,23 @@ public class Scoding_Test6 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String input = "technocredits";
+		String input2 = "hi this is techno gm credits this hi is";
 		Scoding_Test6 test6 = new Scoding_Test6();
 		test6.findFirstNonRepeatch(input);
+		test6.findFirstNonRepeatWord(input2);
 	}
 	
+	void findFirstNonRepeatWord(String input2) {
+		// TODO Auto-generated method stub
+		String[] arr = input2.split(" ");
+		for(int index=0; index<arr.length; index++) {
+			if(input2.indexOf(arr[index])==input2.lastIndexOf(arr[index])) {
+				System.out.println("First non repeating word in '" + input2 + "' is " + arr[index]);
+				break;
+			}
+		}	
+	}
+
 	void findFirstNonRepeatch(String input) {
 		char output='\0';
 		for(int index=0; index<input.length(); index++) {
