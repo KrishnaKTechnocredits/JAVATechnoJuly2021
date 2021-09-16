9d21cb6a59b6801b1db463d1e71763374a5b0283
NehaJoshi <neha19.a.joshi@gmail.com>
2021-09-16 23:12:06 +0530

    Neha_Assignment_34

---
+++ src/neha/assignment_34/TripletProgram1.java
@@ -0,0 +1,50 @@
+package neha.assignment_34;
+
+import java.util.Arrays;
+
+/*program 1 : place 0 after the triple which in sequence.
+
+input = {1,3,4,5,7,3,9,10,11,14,44,67,1,2,3,99}; // 11 + 3 = 14
+output = [1,3,4,5,0,7,3,9,10,11,0,14,44,67,1,2,3,0,99];*/
+
+public class TripletProgram1 {
+	int findTripletCount(int[] arr) {
+		int count = 0;
+		for (int i = 0; i < arr.length - 2; i++) {
+			if (arr[i + 1] == arr[i] + 1 && arr[i + 2] == arr[i] + 2)
+				count++;
+		}
+		return count;
+	}
+
+	void placeZeroAfterTripletAndDisplay(int[] arr) {
+		int outLength = arr.length + findTripletCount(arr); // output array length= original array length+triplet count
+		int[] output = new int[outLength];
+		int outIndex = 0;
+		int inpIndex = 0;
+		while (inpIndex < arr.length) {
+			if (inpIndex != arr.length - 1 && arr[inpIndex + 1] == arr[inpIndex] + 1
+					&& arr[inpIndex + 2] == arr[inpIndex] + 2) {
+				output[outIndex] = arr[inpIndex];
+				output[outIndex + 1] = arr[inpIndex + 1];
+				output[outIndex + 2] = arr[inpIndex + 2];
+				output[outIndex + 3] = 0;
+				inpIndex += 3;
+				outIndex += 4;
+			} else {
+				output[outIndex] = arr[inpIndex];
+				inpIndex++;
+				outIndex++;
+			}
+		}
+		System.out.println(Arrays.toString(output));
+
+	}
+
+	public static void main(String[] args) {
+		TripletProgram1 tripletProgram1 = new TripletProgram1();
+		int[] input = { 1, 3, 4, 5, 7, 3, 9, 10, 11, 14, 44, 67, 1, 2, 3, 99 };
+		tripletProgram1.placeZeroAfterTripletAndDisplay(input);
+	}
+
+}
---
+++ src/neha/assignment_34/TripletProgram2.java
@@ -0,0 +1,44 @@
+package neha.assignment_34;
+/*program 2 : place sum of triple after each triple which in sequence.
+
+input = {1,3,4,5,7,3,9,10,11,14,44,67,1,2,3,99};
+output = [1,3,4,5,12,7,3,9,10,11,30,14,44,67,1,2,3,6,99];*/
+
+import java.util.Arrays;
+
+public class TripletProgram2 extends TripletProgram1 {
+	void placeTripletSumAfterTripletAndDisplay(int[] arr, int tripleCount) {
+		int outLength = arr.length + tripleCount; // output array length= original array
+													// length+triplet count
+		int[] output = new int[outLength];
+		int outIndex = 0;
+		int inpIndex = 0;
+		int sum = 0;
+		while (inpIndex < arr.length) {
+			if (inpIndex != arr.length - 1 && arr[inpIndex + 1] == arr[inpIndex] + 1
+					&& arr[inpIndex + 2] == arr[inpIndex] + 2) {
+				output[outIndex] = arr[inpIndex];
+				output[outIndex + 1] = arr[inpIndex + 1];
+				output[outIndex + 2] = arr[inpIndex + 2];
+				sum = output[outIndex] + output[outIndex + 1] + output[outIndex + 2];
+				output[outIndex + 3] = sum;
+				inpIndex += 3;
+				outIndex += 4;
+			} else {
+				output[outIndex] = arr[inpIndex];
+				inpIndex++;
+				outIndex++;
+			}
+		}
+		System.out.println(Arrays.toString(output));
+
+	}
+
+	public static void main(String[] args) {
+		TripletProgram2 tripletProgram2 = new TripletProgram2();
+		int[] input = { 1, 3, 4, 5, 7, 3, 9, 10, 11, 14, 44, 67, 1, 2, 3, 99 };
+		int tripletCount = tripletProgram2.findTripletCount(input); // findTripletCount(input) is parent class method
+		tripletProgram2.placeTripletSumAfterTripletAndDisplay(input, tripletCount);
+	}
+
+}
