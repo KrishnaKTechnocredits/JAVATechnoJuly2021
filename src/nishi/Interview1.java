22bbfdb7e302d8fbe722eab59f705c808b6092d3
Nishi Bharti <iamnishibharti@gmail.com>
2021-09-18 13:13:48 +0530

    Nishi_Interview_q2

---
+++ src/nishi/Interview1.java
@@ -0,0 +1,35 @@
+package nishi;
+
+public class Interview1 {
+	
+	//String i/p = "Harsh"
+	//String output="Hars"
+	
+	public String getWord(String input)
+	{
+		String[] s1=input.split(input);
+		String s="";
+		int count=0;
+		for(int i=0;i<input.length();i++)
+		{
+			char uniqueChar=input.charAt(i);
+			if(i!=input.indexOf(uniqueChar))
+			{
+				count=0;
+			}
+			else if(i==input.indexOf(uniqueChar))
+			{
+				s+=uniqueChar;
+			}
+			
+		}
+		return s;
+	}
+	
+	public static void main(String[] args) {
+		String input="harsh";
+		System.out.println("output :"+new Interview1().getWord(input));
+		
+	}
+
+}
