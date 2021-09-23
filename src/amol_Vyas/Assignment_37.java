5f9055d76c68db651773b0ee9893c39f92b47026
Amol Vyas <amol.vyas2311.com>
2021-09-23 13:58:08 +0530

    AmolVyas_Assignment_37

--- src/amol_Vyas/Assignment_37.java
+++ src/amol_Vyas/Assignment_37.java
@@ -10,15 +10,23 @@ package amol_Vyas;
 import java.util.Arrays;
 
 public class Assignment_37 {
-	
+
 	static String getSecondHighestElementFromString(String input) {
-		String[] output= input.split(" ");
-		Arrays.sort(output);
-		return output[1];
+		String[] output = input.split(" ");
+		String maxWord = "", secondMax = "";
+		for (int index = 0; index < output.length; index++) {
+			if (output[index].length() > maxWord.length()) {
+				secondMax = maxWord;
+				maxWord = output[index];
+			} else if (output[index].length() > secondMax.length())
+				secondMax = output[index];
+		}
+		return secondMax;
 	}
 
 	public static void main(String[] args) {
-		String input= "Welcome to the family";
-		System.out.println("Second Highest element from the given String is "+getSecondHighestElementFromString(input));
+		String input = "Welcome to the family";
+		System.out
+				.println("Second Highest element from the given String is " + getSecondHighestElementFromString(input));
 	}
 }
