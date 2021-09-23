5e304a726a58ada13e54b097169f6a616f1ce345
Precilla Aranha <precillaaranha16@gmail.com>
2021-09-23 17:48:20 +0530

    Precilla_Assignment37

---
+++ src/precilla/assignment37/SecondLargestString.java
@@ -0,0 +1,26 @@
+/*
+ * Assignment 33:To print second largest element
+ */
+package precilla.assignment37;
+
+public class SecondLargestString {
+	public static void main(String[] args) {
+		String str="welcome to the family";
+		System.out.println("Input : "+str);
+		String largest="";
+		String secondLargest="";
+		String array[]=str.split(" ");
+	
+		
+		for(int index=0;index<array.length;index++) {
+			if(array[index].length()>largest.length()) {
+				secondLargest=largest;
+				largest=array[index];
+			}else if(array[index].length()>secondLargest.length()&&array[index].length()<largest.length()) {
+				secondLargest=array[index];
+			}
+		}
+		
+		System.out.print("Output :"+secondLargest);
+	}
+}
