098658c78c6c62f7af95369045164161463d36cd
Akansha Jain <goyanka.akku@gmail.com>
2021-09-24 09:33:52 +0530

    use scanner class for input

--- src/akanksha_Jain/Assignment_38.java
+++ src/akanksha_Jain/Assignment_38.java
@@ -6,15 +6,22 @@ output : Hi Credits Hello GM Neha Globant */
 
 package akanksha_Jain;
 
+import java.util.Scanner;
+
 public class Assignment_38 {
 	
 	public static void main(String[] args) {
-		String str  = "Hi Hello Techno Hi Credits Hello GM Neha Globant Aashvi Globant";
-		String s1 = "Hi";
-		String s2 = "Globant";
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a string");
+		String str = sc.nextLine(); //str  = "Hi Hello Techno Hi Credits Hello GM Neha Globant Aashvi Globant";
+		System.out.println("Enter the beginning index string");
+		String s1 = sc.next(); // s1 = "Hi";
+		System.out.println("Enter the beginning index string");
+		String s2 = sc.next(); // s2 = "Globant";
 		int beginIndex = str.lastIndexOf(s1);
 		int endIndex = str.indexOf(s2);
 		int length = s2.length();
 		System.out.println(str.substring(beginIndex, endIndex+length));
+		sc.close();
 	}
 }
