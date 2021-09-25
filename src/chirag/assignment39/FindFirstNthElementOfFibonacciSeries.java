815c8321e2828393f787c536cda48d49c96128f6
Chirag Prajapati <chirag.68.cp@gmail.com>
2021-09-25 22:12:18 +0530

    Chirag_Assignment_39

---
+++ src/chirag/assignment39/FindFirstNthElementOfFibonacciSeries.java
@@ -0,0 +1,29 @@
+package chirag.assignment39;
+
+/*Assignment-39 : 25th Sep'2021
+
+Print first N elements of the Fibonacci series. 
+n -> 8
+output : 0,1,1,2,3,5,8,13 
+*/
+
+public class FindFirstNthElementOfFibonacciSeries {
+	
+	void getNthElementOfFibonacciSeries(int num) {
+		int num1=0;
+		int num2=1;
+		int sum=0;
+		System.out.print("Fibonacci Series : "+num1);
+		System.out.print(","+num2);
+		for(int index = 0 ; index < num -2 ; index++) {
+			sum=num1+num2;
+			System.out.print(","+sum);
+			num1=num2;
+			num2=sum;
+		}
+	}
+	public static void main(String[] args) {
+		FindFirstNthElementOfFibonacciSeries nthElement = new FindFirstNthElementOfFibonacciSeries();
+		nthElement.getNthElementOfFibonacciSeries(8);
+	}
+}
