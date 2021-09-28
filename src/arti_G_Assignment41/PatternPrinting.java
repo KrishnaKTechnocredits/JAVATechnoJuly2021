33243a8879d594e55610d6183c24c6dd1e00e5aa
Arti Gahlot <artisaini23101993@gmail.com>
2021-09-28 22:01:51 +0530

    Arti_G_Assignment41

---
+++ src/arti_G_Assignment41/PatternPrinting.java
@@ -0,0 +1,65 @@
+package arti_G_Assignment41;
+/*Assignment-41 : 28th Sep'2021  
+Write a code to print below pattern.
+
+*
+##
+***
+####
+***** */
+
+public class PatternPrinting {
+
+	// pattern1 *
+	// **
+	// ***
+	// ****
+	private void pattern1() {
+		for (int i = 1; i <= 4; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+	// pattern2 1
+	// 12
+	// 123
+	// 1234
+	private void pattern2() {
+		for (int i = 1; i <= 4; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(j);
+				// System.out.println(i);
+			}
+			System.out.println();
+		}
+	}
+
+	/*
+	 * * ##
+	 ***
+	 * ####
+	 */
+	private void pattern3() {
+		for (int i = 1; i <= 4; i++) {
+			for (int j = 1; j <= i; j++) {
+				if (i % 2 == 0) {
+					System.out.print("#");
+				} else {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		PatternPrinting p1 = new PatternPrinting();
+		p1.pattern1();
+		p1.pattern2();
+		p1.pattern3();
+	}
+}
