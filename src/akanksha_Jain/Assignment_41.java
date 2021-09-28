0079a56ca94a5cb6847196bf1f418a76c6862e0a
Akansha Jain <goyanka.akku@gmail.com>
2021-09-28 15:29:30 +0530

    AkanshaJain Assignment 41

---
+++ src/akanksha_Jain/Assignment_41.java
@@ -0,0 +1,28 @@
+/* Assignment-41 : 28th Sep'2021  
+Write a code to print below pattern.
+*
+##
+***
+####
+***** */ 
+
+package akanksha_Jain;
+
+public class Assignment_41 {
+	
+	static void patternPrinting() {
+		for(int i=1; i<=5; i++) {
+			for(int j=1; j<=i; j++) {
+				if(i%2 != 0)
+					System.out.print("*");
+				else
+					System.out.print("#");
+			}
+			System.out.println("\n");
+		}
+	}
+	
+	public static void main(String[] args) {
+		patternPrinting();
+	}
+}
