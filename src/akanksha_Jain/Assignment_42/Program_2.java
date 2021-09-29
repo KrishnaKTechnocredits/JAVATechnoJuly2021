48bfa65e7bff52f403be2d23ded542c06f607c74
Akansha Jain <goyanka.akku@gmail.com>
2021-09-29 11:45:54 +0530

    AkanshaJain_Assignment_42

---
+++ src/akanksha_Jain/Assignment_42/Program_2.java
@@ -0,0 +1,27 @@
+/* Assignment - 42 : 29th Sep'2021 
+Program 2: Remove all occurrence of duplicate character from given string.
+input : "technocredits"
+output : "hnordis */
+
+package akanksha_Jain.Assignment_42;
+
+public class Program_2 {
+	
+	void removeDuplicateOccurence(String str) {
+		System.out.println("After removing duplicate characters from string '" + str + "' -");
+		System.out.print("Output is- ");
+		for(int index=0; index<str.length(); index++) {
+			char ch = str.charAt(index);
+			if(str.indexOf(ch)==str.lastIndexOf(ch)) {
+				System.out.print(ch);
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		String input = "technocredits";
+		Program_2 program_2 = new Program_2();
+		program_2.removeDuplicateOccurence(input);
+	}
+
+}
