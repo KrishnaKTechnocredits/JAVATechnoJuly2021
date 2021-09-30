a4fadcbfbf5fa57c08167e699b51390b10666d41
Amol Vyas <amol.vyas2311.com>
2021-09-30 11:28:32 +0530

    AmolVyas_Assignment_43

---
+++ src/amol_Vyas/Assignment_43.java
@@ -0,0 +1,29 @@
+/*WAP to find length of String without using Length Method and length property.
+String input="technocredits";
+output=13;*/
+
+
+package amol_Vyas;
+
+public class Assignment_43 {
+	
+	static int getLengthOfString(String input) {
+		int count=0;
+		while(true) {
+			try {
+				input.charAt(count);
+				count++;
+			} catch(StringIndexOutOfBoundsException se) {
+				break;
+			}
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+		String input="technocredits";
+		System.out.println("Length of '"+input+"' string is: "+Assignment_43.getLengthOfString(input));
+		
+		
+	}
+}
