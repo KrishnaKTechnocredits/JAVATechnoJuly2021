93aee8c089a2ded6d4b848d623c6040425ea82db
Akansha Jain <goyanka.akku@gmail.com>
2021-10-11 01:21:39 +0530

    AkanshaJain_Assignment_48

---
+++ src/akanksha_Jain/Assignment_48/Program_2.java
@@ -0,0 +1,34 @@
+/* Assignment - 48 : 9th Oct'2021
+Program 2 : Find how many pairs of socks we can have from given array.
+input : [10,3,10,4,7,4,10,3,2,7]
+output : 4 pairs
+Hint : Map */
+
+package akanksha_Jain.Assignment_48;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Program_2 {
+	
+	void findPairs(int[] input) {
+		int count = 0;
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for(int number : input) {
+			if(map.containsKey(number))
+				map.put(number, map.get(number)+1);
+			else
+				map.put(number, 1);
+		}
+		for(int key : map.keySet()) {
+			count += map.get(key)/2;
+		}
+		System.out.println("The number of pairs in " + Arrays.toString(input) + " array is " + count);
+	}
+
+	public static void main(String[] args) {
+		int[] input = {10,3,10,4,7,4,10,3,2,7};
+		new Program_2().findPairs(input);
+	}
+}
