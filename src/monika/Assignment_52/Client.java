736045165ca2484e0f1b2eee8417bfaec27d41f2
Monika Zankar <monikazankar@gmail.com>
2021-10-11 22:19:49 +0530

    Monika_Assignment_52

---
+++ src/monika/Assignment_52/Client.java
@@ -0,0 +1,33 @@
+/*Assignment - 52 : 10th Oct'2021 [Nested Comparision - Comparable -Asc]
+Create a class Called Employee.
+Employee - id, name, salary, years of exp
+Sort Employee object based on name, if name is same next comparision should be years of exp, if years of exp is same, further comparision should be based on salary.
+Sort Employee object based on name, if name is same next comparision should be years of exp, if years of exp is same, further comparision should be based on salary.
+ */
+package monika.Assignment_52;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Client {
+
+	public static void main(String[] args) {
+		Employee emp1 = new Employee(5, "ABC", 2, 400);
+		Employee emp2 = new Employee(6, "ABC", 2, 200);
+		Employee emp3 = new Employee(1, "DEF", 4, 150);
+		Employee emp4 = new Employee(2, "XYZ", 2, 50);
+		Employee emp5 = new Employee(3, "ABC", 1, 800);
+
+		List<Employee> list = new ArrayList<>();
+		list.add(emp1);
+		list.add(emp2);
+		list.add(emp3);
+		list.add(emp4);
+		list.add(emp5);
+		Collections.sort(list);
+		System.out.println(list);
+	}
+
+}
+
