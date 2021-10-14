3023d463b3c6d4f4da71a8474f7b74e4b67a8e2e
Nishi Bharti <iamnishibharti@gmail.com>
2021-10-15 00:21:27 +0530

    Nishi_Assignment_50

---
+++ src/nishi/Assignment50/Client.java
@@ -0,0 +1,23 @@
+package nishi.Assignment50;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Client {
+	public static void main(String[] args) {
+		Builder b1=new Builder("Nishi","GSTWBQ11",10,10000);
+		Builder b2=new Builder("Rahul","GSTWBQ12",5,20000);
+		Builder b3=new Builder("Saurav","GSTWBQ13",20,40000);
+		Builder b4=new Builder("Nishi","GSTWBQ14",11,12000);
+		Builder b5=new Builder("Nishi","GSTWBQ15",2,9000);
+		ArrayList<Builder> al=new ArrayList<Builder>();
+		al.add(b1);
+		al.add(b2);
+		al.add(b3);
+		al.add(b4);
+		al.add(b5);
+		Collections.sort(al);
+		System.out.println(al);
+	}
+	
+}
