af5110af6d9f22d00ef02b87f3646b4b9635934a
Manaswi Humbe <manaswi1010@gmail.com>
2021-10-14 20:33:52 +0530

    Manaswi_Assignment_47_1&2

---
+++ src/manaswi/Assignment_47_2.java
@@ -0,0 +1,32 @@
+package manaswi;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/*Program 2: find frequency of each word from line
+i/p : "hi good morning hello good morning"
+o/p : hi = 1
+	  good = 2
+	  morning = 2
+	  hello = 1*/
+public class Assignment_47_2 
+{
+	public static void main(String[] args) 
+	{
+		String str="hi good morning hello good morning";
+		String[] arr=str.split(" ");
+		System.out.println("Input : "+str);
+		Map<String,Integer> map=new LinkedHashMap<String,Integer>();
+		for(int index=0;index<arr.length;index++) 
+		{
+			if(map.containsKey(arr[index]))
+			{
+				int count=map.get(arr[index]);
+				map.put(arr[index], count+1);
+			}
+			else
+			map.put(arr[index], 1);
+		}
+		System.out.println("Output : "+map);
+	}
+}
