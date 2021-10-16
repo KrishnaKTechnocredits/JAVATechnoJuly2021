f73a2e84f5ba10d0ea75348adc36b7f52c4e899c
Suruchi Joshi <surujos254@gmail.com>
2021-10-16 13:30:06 +0530

    Suruchi_Assignment_50

--- src/suruchi/Assignment50/Client.java
+++ src/suruchi/Assignment50/Client.java
@@ -14,10 +14,10 @@ import java.util.Collections;
 public class Client {
 
 	public static void main(String[] args) {
-		Builder builder2 = new Builder("Amol", "ABS123", 4, 40000);
-		Builder builder3 = new Builder("Ram", "BS123", 6, 12000);
-		Builder builder1 = new Builder("Amol", "ABS123", 4, 2000);
-		Builder builder4 = new Builder("Shyam", "TSBS123", 10, 100000);
+		Builder builder2 = new Builder("Suruchi", "ABS123", 4, 40000);
+		Builder builder3 = new Builder("Sasha", "BS123", 6, 12000);
+		Builder builder1 = new Builder("Suruchi", "ABS123", 4, 2000);
+		Builder builder4 = new Builder("Priya", "TSBS123", 10, 100000);
 		Builder builder5 = new Builder("Techno", "BSABS123", 9, 80000);
 
 		List<Builder> List = new ArrayList<>();
