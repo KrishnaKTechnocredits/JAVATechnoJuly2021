dfb274d42ebd51f0702bfa070ace0acba3b50ff8
Aniket Balsaraf <aniketbalsaraf08@gmail.com>
2021-10-18 18:51:09 +0530

    Assignment_51

---
+++ src/aniket/assignment_51/Client_51.java
@@ -0,0 +1,27 @@
+package aniket.assignment_51;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Client_51 {
+	public static void main(String[] args) 
+	{
+		Builder_51 b1 = new Builder_51("Aniket","ABZ12398IN",11,19000000); 
+		Builder_51 b2 = new Builder_51("Sonu","AHR12398IN",19,9000000); 
+		Builder_51 b3 = new Builder_51("Balsaraf","AHGG145IC",16,1000000); 
+		Builder_51 b4 = new Builder_51("Techno","GFS654TGF",20,5000000); 
+		Builder_51 b5 = new Builder_51("Credits","TDSH145AY",14,8000000); 
+
+		ArrayList<Builder_51> alist = new ArrayList<>();
+		alist.add(b1);
+		alist.add(b2);
+		alist.add(b3);
+		alist.add(b4);
+		alist.add(b5);
+
+		Collections.sort(alist , new RevenueComparator_51());
+		System.out.println(alist);
+		Collections.sort(alist ,Collections.reverseOrder(new TotalCompleteProjectComparator()));
+		System.out.println(alist);
+	}
+}
