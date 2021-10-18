e9e996cc5108659fc36539dedae886d9c89e835b
Arti Gahlot <artisaini23101993@gmail.com>
2021-10-18 23:41:24 +0530

    Modified

--- src/arti_G_Assignment48/Program1.java
+++ src/arti_G_Assignment48/Program1.java
@@ -6,26 +6,20 @@ import java.util.Map;
 /*Program 1: Create a system to generate AadharNumber with rules discussed in class.*/
 public class Program1 {
 
-	private String panNumber;
-	private static Map<String, String> map = new HashMap<String, String>();
-
-	public Program1(String string) {
-		// TODO Auto-generated constructor stub
+	static HashMap<String,String> panAadharMap=new HashMap<String,String>();
+	String panCard="";
+	Program1(String panCard){
+		this.panCard=panCard;
 	}
+	String generatePancard() {
 
-	public void AadharGenerator(String panNumber) {
-		this.panNumber = panNumber;
+		if(!panAadharMap.containsKey(panCard)) 
+			panAadharMap.put(panCard, panCard+1);
+		return panAadharMap.get(panCard);
+
 	}
-
-	String getAadharNumber() {
-		if (!map.containsKey(panNumber))
-			map.put(panNumber, panNumber + 1);
-		return map.get(panNumber);
-	}	
 	public static void main(String[] args) {
-		Program1 aadharCenter1 = new Program1("FKAPS6730K");
-		System.out.println(aadharCenter1.getAadharNumber());
-		System.out.println("******************2********************");
-	}
-
+		Program1 aadharCenter1 = new Program1("BPABJP7351K");
+		System.out.println(aadharCenter1.generatePancard());
+	}	
 }
