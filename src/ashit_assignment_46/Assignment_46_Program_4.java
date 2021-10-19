35864cb2a40dba701387085a1de0872c3e4a55a1
Ashit46 <asamdur@gmail.com>
2021-10-19 19:17:20 +0530

    Ashit_Assignment_46

---
+++ src/ashit_assignment_46/Assignment_46_Program_4.java
@@ -0,0 +1,28 @@
+/* Assignment 46 - Collections : 4th Oct'2021
+Example 4: Write a program to remove duplicate from ArrayList (using set).
+Make sure element order remain same. */
+
+package ashit_assignment_46;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Assignment_46_Program_4
+ {
+
+	void removeDuplicatesUsingSet(ArrayList<Integer> al) 
+	{
+		Set<Integer> s = new LinkedHashSet<Integer>(al);
+		System.out.println(s + " is the list after removing all duplicates");
+	}
+
+	public static void main(String[] args) 
+	{
+		Integer[] input = {5, 34, 11, 56, 74, 85, 11, 5};
+		ArrayList<Integer> al = new ArrayList<Integer>(Arrays.asList(input));
+		Assignment_46_Program_4 p4 = new Assignment_46_Program_4();
+		p4.removeDuplicatesUsingSet(al);
+	}
+}
