aafae80401aa732b70fab8ebe66753c4ef40fe86
Ritabrata Bhattacharyya <ritabrata.bhattacharyya96@gmail.com>
2021-10-19 21:18:22 +0530

    Ritabrata_Assignment_49

--- src/ritabrata2/ritabrata_assignment_49/Client.java
+++ src/ritabrata2/ritabrata_assignment_49/Client.java
@@ -18,9 +18,9 @@ import java.util.HashSet;
 public class Client {
 
 	public static void main(String[] args) {
-		Builder b1 = new Builder("vivek", "1234a", 100, 100000);
-		Builder b2 = new Builder("vivek", "1234a", 100, 100000);
-		Builder b3 = new Builder("patil", "4321b", 101, 90000);
+		Builder b1 = new Builder("Ritabrata", "1234a", 100, 100000);
+		Builder b2 = new Builder("Ritabrata", "1234a", 100, 100000);
+		Builder b3 = new Builder("Ritabrata", "4321b", 101, 90000);
 		Builder b4 = new Builder("techno", "5678c", 102, 80000);
 		Builder b5 = new Builder("credits", "8765d", 103, 70000);
 		
